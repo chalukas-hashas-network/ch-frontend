0d64153c713bc4d8897f7e11ed09120d495ad557
Benny Sufrin <bennysuf@gmail.com>
2025-02-07 16:34:56 -0500

    add settings and admin routes

--- src/components/Routing.js
+++ src/components/Routing.js
@@ -2,13 +2,28 @@ import { Routes, Route } from "react-router-dom";
 // import Home from "./Home.js";
 import Login from "./Login.js";
 import Profile from "./Profile.js";
+import Settings from "./Settings.js";
+
 export default function Routing() {
+    const auth = true;
+    const admin = false;
+    
     return (
         <Routes>
             {/* <Route path="/" element={<Home />} /> */}
             <Route path="/login/*" element={<Login />} />
             <Route path="/login/invite/:userEmail/:CommunityID" element={<Login />} />
-            <Route path="/profile" element={<Profile />} />
+            {auth && (
+                <>
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/settings" element={<Settings />} />
+                {/* {admin && (
+                    <Route path="dashboard" element={<Dashboard/>} />
+                )} */}
+                </>
+            )}
+            {/* <Route path="*" element={<NotFound />} /> */}
+
         </Routes>
     );
  }
