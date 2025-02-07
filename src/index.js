1a9ca9b22e7bc898c978091c3ec7a097ca9b1dc4
Benny Sufrin <bennysuf@gmail.com>
2025-02-07 15:12:38 -0500

    add report vitals, remove files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./components/App.js";
+import reportWebVitals from "./reportWebVitals.js";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
