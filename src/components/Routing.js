6b9c27416d1ff6aac54e960d95aa4cac04c93c55
Benny Sufrin <bennysuf@gmail.com>
2025-02-13 14:00:56 -0500

    add extra login routes

--- src/components/Routing.js
+++ src/components/Routing.js
@@ -3,27 +3,35 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./Login.js";
 import Profile from "./Profile.js";
 import Settings from "./Settings.js";
+import AdminDash from "./AdminDash.js";
+import Community from "./Community.js";
+import Goal from "./Goal.js";
 
 export default function Routing() {
-    const auth = true;
-    const admin = false;
-    
-    return (
-        <Routes>
-            {/* <Route path="/" element={<Home />} /> */}
-            <Route path="/login/*" element={<Login />} />
-            <Route path="/login/invite/:userEmail/:CommunityID" element={<Login />} />
-            {auth && (
-                <>
-                <Route path="/profile" element={<Profile />} />
-                <Route path="/settings" element={<Settings />} />
-                {/* {admin && (
-                    <Route path="dashboard" element={<Dashboard/>} />
-                )} */}
-                </>
-            )}
-            {/* <Route path="*" element={<NotFound />} /> */}
+  const auth = true;
+  const admin = true;
 
-        </Routes>
-    );
- }
+  return (
+    <Routes>
+      {/* <Route path="/" element={<Home />} /> */}
+      <Route path="/login/*" element={<Login />} />
+      <Route path="/login/invite/:CommunityID" element={<Login />} />
+      {/* <Route path="/login/invite/:userEmail/:CommunityID" element={<Login />} /> */}
+      <Route path="/community" element={<Community />} />
+      <Route path="/goal" element={<Goal />} />
+      {auth ? (
+        <>
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/settings" element={<Settings />} />
+        </>
+      ) : (
+        <>
+          <Route path="/profile" element={<Login />} />
+          <Route path="/settings" element={<Login />} />
+        </>
+      )}
+      {admin && <Route path="/dashboard" element={<AdminDash />} />}
+      {/* <Route path="*" element={<NotFound />} /> */}
+    </Routes>
+  );
+}
