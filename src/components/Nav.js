3ea998aa84ab18db4a1198dc4eb1cd66613bebf1
Benny Sufrin <bennysuf@gmail.com>
2025-02-19 23:57:32 -0500

    add logout button and trigger logout logic

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,23 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { Button } from "@mui/material";
+import { useUser } from "../utils/Context";
 
 function Nav() {
+  const {logout} = useUser();
+  const navigate = useNavigate();
+
+  const handleLogout = () =>{
+    logout()
+    localStorage.clear()
+    navigate("/login")
+  }
+
   return (
     <>
       {/* <Link style={{ textDecoration: "none", color: "black" }} to="/profile">
         {"< Back"}
       </Link> */}
+      <Button onClick={handleLogout}>logout</Button>
     </>
   );
 }
