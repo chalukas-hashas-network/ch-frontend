c46a1ae2e7c822528d2bcdbcd902da63a1b4c3d3
Benny Sufrin <bennysuf@gmail.com>
2025-02-20 00:20:48 -0500

    start adding tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,33 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+
+// /*
+import {getAccessToken} from "./utils/API/AuthAPI";
+import {getUser} from "./utils/API/UserAPI";
+const user = {username: "levi", password: "12345"};
+let userData = {}
+test('renders user', () => {
+
+  getAccessToken(user.username, user.password).then(() => {
+    getUser().then((data) => {
+      console.log("user data", data);
+      userData = data;
+    });
+  });
+
+  expect(userData).not.toBeUndefined();
+  expect(userData.username).toBe(user.username);
+
+  //TODO: more tests for user data and other functionalities. like communitiees
+  // take community[0] and use the id to query in the next fetch to be tested (select community)
+// trigger all endpoints as such
+// at the end, localStroage.clear();
+})
+//  */
