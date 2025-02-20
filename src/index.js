cd675bbafcb42ce5888be6915a791cfd2fd1d06f
Benny Sufrin <bennysuf@gmail.com>
2025-02-19 23:56:06 -0500

    add user context to cover routes

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./components/App.js";
 import reportWebVitals from "./reportWebVitals.js";
+import { UserProvider } from "./utils/Context.js";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-); 
+  <UserProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </UserProvider>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
