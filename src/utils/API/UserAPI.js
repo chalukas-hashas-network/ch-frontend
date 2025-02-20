368d4b858eeda61b6b4dd909c258508f75d73007
Benny Sufrin <bennysuf@gmail.com>
2025-02-20 00:03:56 -0500

    move file, clean up, fix routes

---
+++ src/utils/API/UserAPI.js
@@ -0,0 +1,77 @@
+import { ACCESS_TOKEN, REFRESH_TOKEN } from "../Authorization";
+
+const API_URL = process.env.REACT_APP_API_URL;
+
+const headers = {
+  "Content-Type": "application/json",
+};
+
+export const getUser = async () => {
+  const token = localStorage.getItem(ACCESS_TOKEN);
+
+  if (token) {
+    headers["Authorization"] = `Bearer ${token}`;
+  }
+
+  try {
+    const response = await fetch(API_URL + "/me/", {
+      method: "GET",
+      headers: headers,
+    });
+    if (!response.ok) {
+      throw new Error(`Error: ${response.statusText}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    // setError(err.message);  // Set the error message
+  } finally {
+    // setLoading(false);  // Set loading to false once the fetch is complete
+  }
+};
+
+export const createUsers = async (body) => {
+  console.log("hit create user request");
+  try {
+    const response = await fetch(API_URL + "/user/register/", {
+      method: "POST",
+      // headers,
+      body: JSON.stringify({
+        body,
+      }),
+    });
+    if (!response.ok) {
+      throw new Error(`Error: ${response.statusText}`);
+    }
+
+    return await response.json();
+    // setUser(data);  // Set the fetched user data
+  } catch (err) {
+    // setError(err.message);  // Set the error message
+  } finally {
+    // setLoading(false);  // Set loading to false once the fetch is complete
+  }
+};
+
+export const updateUser = async (body) => {
+  console.log("hit update user request");
+  try {
+    const response = await fetch(API_URL + "/me/update/", {
+      method: "PATCH",
+      // headers,
+      body: JSON.stringify({
+        body,
+      }),
+    });
+    if (!response.ok) {
+      throw new Error(`Error: ${response.statusText}`);
+    }
+
+    return await response.json();
+    // setUser(data);  // Set the fetched user data
+  } catch (err) {
+    // setError(err.message);  // Set the error message
+  } finally {
+    // setLoading(false);  // Set loading to false once the fetch is complete
+  }
+};
