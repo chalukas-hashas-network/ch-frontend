f308614a4a61b3fe1d9af5aad8a87d46cd6a39b3
Benny Sufrin <bennysuf@gmail.com>
2025-02-20 00:06:32 -0500

    create file, add create and refresh token fetches

---
+++ src/utils/API/AuthAPI.js
@@ -0,0 +1,46 @@
+import { ACCESS_TOKEN, REFRESH_TOKEN } from "../Authorization";
+
+const API_URL = process.env.REACT_APP_API_URL;
+
+export const getAccessToken = async (username, password) => {
+  try {
+    const response = await fetch(API_URL + "/token/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username: username, password: password }),
+    });
+    if (!response.ok) {
+      throw new Error(`Error: ${response.statusText}`);
+    }
+    const data = await response.json();
+    localStorage.setItem(ACCESS_TOKEN, data.access);
+    localStorage.setItem(REFRESH_TOKEN, data.refresh);
+    return data;
+  } catch (error) {
+    console.log("userLogin error: " + error);
+  }
+};
+
+export const refreshAccessToken = async () => {
+  const refreshToken = localStorage.getItem(REFRESH_TOKEN);
+
+  try {
+    const response = await fetch(API_URL + "/token/refresh/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ refresh: refreshToken }),
+    });
+    if (!response.ok) {
+      throw new Error(`Error: ${response.statusText}`);
+    }
+    const data = await response.json();
+    localStorage.setItem(ACCESS_TOKEN, data.access);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
