3be22beacfc9ea5ed66c146dc535dcfc420f1013
Benny Sufrin <bennysuf@gmail.com>
2025-02-22 22:42:56 -0500

    update getCommunity and make url dynamic

--- src/utils/API/CommunityAPI.js
+++ src/utils/API/CommunityAPI.js
@@ -1,24 +1,4 @@
-// Community admin register setup.
-
-// {
-// communityAdmin: true,
-// username: “Benny”,
-// email: “benny@something.com”,
-// phoneNumber: “717177171”,
-// communityId: parseInt(1),
-// password: “12345”,
-// passwordConfirmation: “12345”
-// }
-
-// Regular user register
-// {
-// username: “Benny”,
-// email: “benny@something.com”,
-// phoneNumber: “717177171”,
-// communityId: parseInt(1),
-// password: “12345”,
-// passwordConfirmation: “12345”
-// }
+import { ACCESS_TOKEN } from "../Authorization";
 
 const API_URL = process.env.REACT_APP_API_URL;
 
@@ -43,9 +23,45 @@ export const getCommunities = async () => {
     // setLoading(false);  // Set loading to false once the fetch is complete
   }
 };
-export const getCommunity = async (communityName) => {
+export const getCommunity = async (
+  filterField,
+  filterValue,
+  includeFields=[],
+  communityName,
+  queryType
+) => {
+  const token = localStorage.getItem(ACCESS_TOKEN);
+
+  let url = API_URL + "/admin-query/communities/?";
+  const queryParams = [];
+
+  // Add the filter parameter to the query string if it's provided
+  if (filterField && filterValue) {
+    queryParams.push(`by_${filterField}=${filterValue}`);
+  }
+
+  // Add each include parameter to the query string
+  includeFields.forEach((field) => {
+    queryParams.push(`${field}=true`);
+  });
+
+  // If there are query parameters, append them to the URL
+  if (queryParams.length > 0) {
+    url += queryParams.join("&");
+  }
+
+  if (token) {
+    headers["Authorization"] = `Bearer ${token}`;
+  }
   try {
-    const response = await fetch(API_URL + "/communities/select");
+    const response = await fetch(
+      // API_URL + "/admin-query/communities/?" + queryType + "=" + communityName,
+      url,
+      {
+        method: "GET",
+        headers: headers,
+      }
+    );
     // Check if the response is OK
     if (!response.ok) {
       throw new Error(`Error: ${response.statusText}`);
@@ -108,14 +124,17 @@ export const createCommunity = async (name, location) => {
 
 export const updateCommunity = async (name, location, id) => {
   try {
-    const response = await fetch(API_URL + "/communities/update/community_id/", {
-      method: "PATCH",
-      headers: headers,
-      body: JSON.stringify({
-        name: name,
-        location: location,
-      }),
-    });
+    const response = await fetch(
+      API_URL + "/communities/update/community_id/",
+      {
+        method: "PATCH",
+        headers: headers,
+        body: JSON.stringify({
+          name: name,
+          location: location,
+        }),
+      }
+    );
     if (!response.ok) {
       // throw new Error(`Error: ${response.statusText}`);
     }
