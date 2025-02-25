91ce78cdaf618cff69e6d97f092f2a877043076f
Benny Sufrin <bennysuf@gmail.com>
2025-02-24 21:58:12 -0500

    add not found page, make routes exact, add isLoading over routes

--- src/utils/Routing.js
+++ src/utils/Routing.js
@@ -6,30 +6,34 @@ import AdminDash from "../components/AdminDash.js";
 import Community from "../components/Community.js";
 import Goal from "../components/Goal.js";
 import { useUser } from "./Context.js";
+import NotFound from "../components/NotFound.js";
 
 export default function Routing() {
-  const {isAdmin, isAuth} = useUser()
+  const { isAdmin, isAuth, isLoading } = useUser();
 
   return (
-    <Routes>
-      <Route path="/login/*" element={<Login />} />
-      <Route path="/login/invite/:CommunityID" element={<Login />} />
-      <Route path="/community" element={<Community />} />
-      <Route path="/goal" element={<Goal />} />
-      {isAuth ? (
-        <>
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/settings" element={<Settings />} />
-        </>
-      ) : (
-        <>
-          <Route path="/profile" element={<Login />} />
-          <Route path="/settings" element={<Login />} />
-        </>
+    <>
+      {!isLoading && (
+        <Routes>
+          <Route path="/login/*" element={<Login />} />
+          <Route exact path="/login/invite/:CommunityID" element={<Login />} />
+          <Route exact path="/community" element={<Community />} />
+          <Route exact path="/goal" element={<Goal />} />
+          {isAuth ? (
+            <>
+              <Route exact path="/profile" element={<Profile />} />
+              <Route exact path="/settings" element={<Settings />} />
+            </>
+          ) : (
+            <>
+              <Route path="/profile" element={<Login />} />
+              <Route path="/settings" element={<Login />} />
+            </>
+          )}
+          {isAdmin && <Route exact path="/dashboard" element={<AdminDash />} />}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       )}
-      {isAdmin && <Route path="/dashboard" element={<AdminDash />} />}
-      {/* <Route path="*" element={<NotFound />} /> */}
-      {/* {isLoading && <Route path="*" element={<Loading />} />} */}
-    </Routes>
+    </>
   );
 }
