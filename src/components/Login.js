692a7118658353fc60854f0f6431393d5ad6d925
Benny Sufrin <bennysuf@gmail.com>
2025-02-24 22:06:10 -0500

    update forms, add signup logic

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,21 +1,29 @@
 // forgot password
 // need to check if user is already signed up
 import { useState, useEffect } from "react";
-import { useLocation, useParams, useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { useUser } from "../utils/Context";
-import {Button} from "../utils/dataExports/muiExports";
+import { Button } from "../utils/dataExports/muiExports";
+import states from "../utils/dataExports/StatesExports";
+
 
 function Login() {
-  const { CommunityID } = useParams();
   const location = useLocation();
-  const { login } = useUser();
+  const { login, triggerLoading } = useUser();
   const navigate = useNavigate();
 
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  const [signupPopup, setSignupPopup] = useState(false);
   const [userStatus, setUserStatus] = useState("Login");
-  const [community, setCommunity] = useState(CommunityID || "");
-  const [email, setEmail] = useState("");
+  const [userData, setUserData] = useState({
+    email: "",
+    username: "",
+    password: "",
+    confirmPassword: "",
+    phone_number: "",
+    first_name: "",
+    last_name: "",
+    location: "",
+  });
 
   useEffect(() => {
     if (location.pathname.startsWith("/login/invite/")) {
@@ -23,80 +31,208 @@ function Login() {
     }
   }, [location.pathname]);
 
-  const handleLogin = () => {
-    login(username, password).then(() => {
-      navigate("/profile");
-    });
+  const handleChange = (e) => {
+    setUserData({ ...userData, [e.target.name]: e.target.value });
   };
 
   const handleSignup = () => {
-    console.log("signing up");
+    if (userData.password !== userData.confirmPassword) {
+      alert("Passwords do not match");
+      return;
+    }
+    if (userData.email === "") {
+      alert("Please enter an email");
+      return;
+    }
+    if (userData.password === "" || userData.confirmPassword === "") {
+      alert("Please enter a password");
+      return;
+    }
+    setSignupPopup(true);
   };
 
   const handleFormToggle = () => {
-    if (userStatus === "Login") {
-      setUserStatus("Signup");
-    } else {
-      setUserStatus("Login");
+    setUserStatus(userStatus === "Login" ? "Signup" : "Login");
+  };
+
+  const handleSubmit = async (e) => {
+    if (e === "Login") {
+      try {
+        await login(userData.username, userData.password);
+        triggerLoading();
+        navigate("/profile");
+      } catch (err) {
+        alert("Invalid credentials");
+        return;
+      }
+    } else if (e === "Register user") {
+      // handleSignup();
+      let missingFields = [];
+
+      // Collect all the missing fields
+      for (let key in userData) {
+        if (userData[key] === "") {
+          missingFields.push(key.replace("_", " "));
+        }
+      }
+      // If any fields are missing, alert them
+      if (missingFields.length > 0) {
+        alert(
+          `Please fill out the following fields: ${missingFields.join(", ")}`
+        );
+        return; // Exit the function early
+      }
+      console.log("Registering user", userData);
+      // navigate("/profile");
+      try {
+        // const user = await createUser(userData);
+        // console.log("User created", user);
+        navigate("/profile");
+      } catch (err) {
+        console.error("Error creating user", err);
+        alert("Error creating user");
+        return;
+      }
     }
   };
 
   return (
     <>
-      <nav style={{ marginTop: "3%" }}>
-        <strong>Codify</strong>
-      </nav>
-      <article className="input" style={{ marginTop: "10px" }}>
-        {userStatus === "Signup" && (
-          <form>
-            <input
-              type="text"
-              placeholder="Community"
-              value={community}
-              onChange={(e) => setCommunity(e.target.value)}
-            />
-            <input
-              type="email"
-              name="email"
-              placeholder="Email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </form>
-        )}
-        <input
-          type="text"
-          placeholder="Username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <br />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <br />
-        {userStatus === "Login" ? (
-          <Button type="button" onClick={handleLogin}>
-            {userStatus}
-            {/* nav to users profile */}
+      <div>
+        <nav style={{ marginTop: "3%" }}>
+          <strong>Codify</strong>
+        </nav>
+        <article className="input" style={{ marginTop: "10px" }}>
+          {userStatus === "Login" && (
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+                handleSubmit("Login");
+              }}
+            >
+              <input
+                type="text"
+                placeholder="Username"
+                name="username"
+                value={userData.username}
+                onChange={handleChange}
+              />
+              <br />
+              <input
+                type="password"
+                placeholder="Password"
+                name="password"
+                value={userData.password}
+                onChange={handleChange}
+              />
+              <Button type="submit">Login</Button>
+            </form>
+          )}
+
+          {userStatus === "Signup" && (
+            <div>
+              <form
+                onSubmit={(e) => {
+                  e.preventDefault();
+                  // handleSubmit("Register user");
+                  handleSignup();
+                }}
+              >
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                  value={userData.email}
+                  onChange={handleChange}
+                />
+                <br />
+                <input
+                  type="password"
+                  placeholder="Password"
+                  name="password"
+                  value={userData.password}
+                  onChange={handleChange}
+                />
+                <br />
+                <input
+                  type="password"
+                  placeholder="Confirm Password"
+                  name="confirmPassword"
+                  value={userData.confirmPassword}
+                  onChange={handleChange}
+                />
+                <br />
+                <Button type="submit">Signup</Button>
+              </form>
+            </div>
+          )}
+
+          <Button type="button" onClick={handleFormToggle}>
+            {userStatus === "Login"
+              ? "Create an account"
+              : "Already have an account?"}
           </Button>
-        ) : (
-          <Button type="button" onClick={handleSignup}>
-            {userStatus}
-            {/* nav to users profile */}
-          </Button>
-        )}
-        <br />
-        {/* {<h4 key={errors}>{errors}</h4>} */}
-        <Button type="button" onClick={handleFormToggle}>
-          {userStatus === "Login"
-            ? "Create an account"
-            : "Already have an account?"}
-        </Button>
-      </article>
+        </article>
+      </div>
+      {signupPopup && (
+        <div className="popup-overlay">
+          <div className="popup-card">
+            <button onClick={() => setSignupPopup(false)}>X</button>
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+                handleSubmit("Register user");
+              }}
+            >
+              <h1>Register</h1>
+              <input
+                type="text"
+                name="username"
+                placeholder="Username"
+                value={userData.username}
+                onChange={handleChange}
+              />
+              <input
+                type="text"
+                name="first_name"
+                placeholder="First Name"
+                value={userData.first_name}
+                onChange={handleChange}
+              />
+              <br />
+              <input
+                type="text"
+                name="last_name"
+                placeholder="Last Name"
+                value={userData.last_name}
+                onChange={handleChange}
+              />
+              <br />
+              <input
+                type="tel"
+                name="phone_number"
+                placeholder="Phone Number"
+                value={userData.phone_number}
+                onChange={handleChange}
+              />
+              <br />
+              <select
+                name="location"
+                value={userData.location}
+                onChange={handleChange}
+              >
+                <option value="">Select state</option>
+                {states.map((state, index) => (
+                  <option key={index} value={state}>
+                    {state}
+                  </option>
+                ))}
+              </select>
+              <Button type="submit">Create Account</Button>
+            </form>
+          </div>
+        </div>
+      )}
     </>
   );
 }
