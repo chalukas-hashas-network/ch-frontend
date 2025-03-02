5ebaeacdd42b09e0c38924e826f3dff2e3fed5c0
Benny Sufrin <bennysuf@gmail.com>
2025-03-02 13:54:43 -0500

    remove is auth to render Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,37 @@
 import { Link } from "react-router-dom";
 import Nav from "./Nav";
+import { useUser } from "../utils/Context";
+import { useState } from "react";
+import Login from "./Login";
 
 function Home() {
-    return ( <>
-    <div>event banner</div>
-    <Link style={{ textDecoration: "none", color: "black", width: "100px" }} to="/profile">
-    <div>logo</div>
-    </Link>
-    <Nav />
-    </> );
+  const [loginOpen, setLoginOpen] = useState(false);
+  const { isAuth } = useUser();
+
+  const openLogin = (bool) => {
+    setLoginOpen(bool);
+  };
+
+  return (
+    <>
+      {isAuth && (
+        <div>
+          <div>event banner</div>
+          <Link
+            style={{ textDecoration: "none", color: "black", width: "100px" }}
+            to="/profile"
+          >
+            <div>logo</div>
+          </Link>
+          <Nav />
+        </div>
+      )}
+      {!loginOpen && !isAuth && (
+        <Link onClick={() => setLoginOpen(true)}>Login</Link>
+      )}
+      {loginOpen && <Login openLogin={openLogin} />}
+    </>
+  );
 }
 
 export default Home;
