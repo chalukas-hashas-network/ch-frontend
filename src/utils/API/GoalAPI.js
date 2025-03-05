57895694ee5143578e8cc168f3c1eb7bc60c733f
Benny Sufrin <bennysuf@gmail.com>
2025-03-04 21:53:39 -0500

    add create for goals and goalTractate, and getAllTractates

---
+++ src/utils/API/GoalAPI.js
@@ -0,0 +1,85 @@
+import { ACCESS_TOKEN } from "../Authorization";
+
+const API_URL = process.env.REACT_APP_API_URL;
+
+const headers = {
+  "Content-Type": "application/json",
+};
+
+export const createGoal = async (id, year) => {
+  debugger;
+  const token = localStorage.getItem(ACCESS_TOKEN);
+
+  if (token) {
+    headers["Authorization"] = `Bearer ${token}`;
+  }
+
+  try {
+    const response = await fetch(API_URL + "/goals/create/", {
+      method: "POST",
+      headers: headers,
+      body: JSON.stringify({
+        user_id: id,
+        year: year,
+      }),
+    });
+    if (!response.ok) {
+      // throw new Error(`Error: ${response.statusText}`);
+    }
+
+    return await response.json();
+
+    // setUser(data);  // Set the fetched user data
+  } catch (err) {
+    console.log(err);
+    // setError(err.message);  // Set the error message
+  } finally {
+    // setLoading(false);  // Set loading to false once the fetch is complete
+  }
+};
+
+export const createGoalTractate = async (id, year) => {
+  debugger;
+  const token = localStorage.getItem(ACCESS_TOKEN);
+
+  if (token) {
+    headers["Authorization"] = `Bearer ${token}`;
+  }
+
+  try {
+    const response = await fetch(API_URL + "/goal-tractate/create/", {
+      method: "POST",
+      headers: headers,
+      body: JSON.stringify({
+        goal_id: 0, // [select]
+        tractate_id: 0, // [select]
+        // tractate_pages_completed: 0.0 // [0.0 by default]
+      }),
+    });
+    if (!response.ok) {
+      // throw new Error(`Error: ${response.statusText}`);
+    }
+
+    return await response.json();
+
+    // setUser(data);  // Set the fetched user data
+  } catch (err) {
+    console.log(err);
+    // setError(err.message);  // Set the error message
+  } finally {
+    // setLoading(false);  // Set loading to false once the fetch is complete
+  }
+};
+
+export const getAllTractates = async () => {
+  try {
+    const response = await fetch(API_URL + "/tractates/");
+    if (!response.ok) {
+      throw new Error(`Error: ${response.statusText}`);
+    }
+
+    return await response.json();
+  } catch (e) {
+    console.error("Error fetching tactates:", e);
+  }
+};
