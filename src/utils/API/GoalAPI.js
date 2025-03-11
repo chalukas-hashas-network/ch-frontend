d8397d147a8a4552bcdfc995c096e3ebea83e8ec
Benny Sufrin <bennysuf@gmail.com>
2025-03-10 21:06:00 -0400

    add update tractateGoal method

--- src/utils/API/GoalAPI.js
+++ src/utils/API/GoalAPI.js
@@ -83,3 +83,33 @@ export const getAllTractates = async () => {
     console.error("Error fetching tactates:", e);
   }
 };
+
+export const updateTractateGoal = async (body) => {
+  // goal_tractate_id: int,
+	// tractate_pages_completed: float, // [select]
+	// tractate_id: int //[optional. select]
+  const token = localStorage.getItem(ACCESS_TOKEN);
+
+  if (token) {
+    headers["Authorization"] = `Bearer ${token}`;
+  }
+
+  try {
+    const response = await fetch(API_URL + "/goal-tractate/update/", {
+      method: "PATCH",
+      headers,
+      body: JSON.stringify(body),
+    });
+    if (!response.ok) {
+      throw new Error(`Error: ${response.statusText}`);
+    }
+
+    return await response.json();
+    // setUser(data);  // Set the fetched user data
+  } catch (err) {
+    // setError(err.message);  // Set the error message
+  } finally {
+    // setLoading(false);  // Set loading to false once the fetch is complete
+  }
+};
+
