035962ccf308dcd0df873b1f5db58f445ea243e5
Benny Sufrin <bennysuf@gmail.com>
2025-03-12 23:45:43 -0400

    add dropdown and logic for user progress

--- src/pages/Goal.js
+++ src/pages/Goal.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import GoalPopup from "../components/GoalPopup";
 import { useUser } from "../utils/Context";
-import { createGoal, getAllTractates } from "../utils/API/GoalAPI";
+import { createAnnualGoal, getAllTractates } from "../utils/API/GoalAPI";
 
 /*
 TODO: 
@@ -10,13 +10,19 @@ get all goals (might be array of all years)
 see if theres a goal with goal.year.includes(current.year) (may have to change year check later)
 if not, on submit, create new goal then create new goalTractate
 or have start goal for {current.year} and onclick create goal and open popup for "create goal"
+TODO: build out progress bar, allow for user to delete goal, finish year logic, update user state upon any edits, close popup on submit, fix page dropdown after submit (goes to default)
 */
 
 function Goal() {
   const [openGoal, setOpenGoal] = useState(false);
   const [goalEditOption, setGoalEditOption] = useState("");
   const [tractates, setTractates] = useState([]);
-  const { user } = useUser();
+  const [selectedTractate, setSelectedTractate] = useState({
+    tractate: "total",
+    pages_completed: 0,
+    pages_selected: 0,
+  });
+  const { user, setUser } = useUser();
   const { goal } = user;
 
   useEffect(
@@ -25,6 +31,7 @@ function Goal() {
         try {
           const tractates = await getAllTractates();
           setTractates(tractates);
+          console.log("tractates", tractates);
         } catch (e) {
           console.error("Error fetching tractates:", e);
         }
@@ -52,13 +59,32 @@ function Goal() {
 
   const createGoalForCurrentYear = async () => {
     try {
-      await createGoal(user.id, `${currentYear}-${currentYear + 1}`);
+      await createAnnualGoal(user.id, `${currentYear}-${currentYear + 1}`);
       alert("created new goal for " + currentYear);
       setGoalEditOption("create-goal");
       setOpenGoal(true);
     } catch (e) {
-      console.error("Error creating goal for current year:", e);
+      console.error("Error creating goal for current year: ", e);
       alert("Error creating goal for current year");
+    }
+  };
+
+  const handleTractateChange = (event) => {
+    const selectedGoal = goal.goal_tractates.find(
+      (tractate) => tractate.tractate === event.target.value
+    );
+    if (selectedGoal) {
+      setSelectedTractate({
+        tractate: selectedGoal.tractate,
+        pages_completed: selectedGoal.tractate_pages_completed,
+        pages_selected: selectedGoal.tractate_pages_selected,
+      });
+    } else {
+      setSelectedTractate({
+        tractate: "total",
+        pages_completed: goal?.user_total_completed_pages,
+        pages_selected: goal?.user_total_selected_pages,
+      });
     }
   };
 
@@ -67,13 +93,45 @@ function Goal() {
       <Link style={{ textDecoration: "none", color: "black" }} to="/profile">
         {"< Back"}
       </Link>
-      <div className="card" style={{ height: "40px" }}>
-        User progress bar
-        {/* <div> */}
-        {/* if the user has more than one tractate have a dropdown and allow him to select one */}
-        <>Tractate name:</>
-        <>Total pages complete: {goal?.user_total_completed_pages} / {goal?.user_total_selected_pages}</>
-        {/* </div> */}
+      <div className="card" style={{ height: "100px" }}>
+        <>User Progress Bar </>
+        <br />
+        {user?.goal?.goal_tractates?.length > 1 ? (
+          <div>
+            <select
+              style={{ marginLeft: "10px" }}
+              onChange={handleTractateChange}
+            >
+              <option value="total">Total</option>
+              {user.goal.goal_tractates.map((tractate) => (
+                <option key={tractate.id} value={tractate.tractate}>
+                  {tractate.tractate}
+                </option>
+              ))}
+            </select>
+            <br />
+            Pages completed:{" "}
+            {selectedTractate?.tractate === "total"
+              ? goal?.user_total_completed_pages
+              : selectedTractate.pages_completed}{" "}
+            /{" "}
+            {selectedTractate?.tractate === "total"
+              ? goal?.user_total_selected_pages
+              : selectedTractate.pages_selected}
+          </div>
+        ) : (
+          <div>
+            <span style={{ marginLeft: "10px" }}>
+              {user?.goal?.goal_tractates?.[0]?.tractate ||
+                "No tractate selected"}
+            </span>
+            Pages complete for {user?.goal?.goal_tractates?.[0]?.tractate}:{" "}
+            {goal?.user_total_completed_pages} /{" "}
+            {goal?.user_total_selected_pages}
+          </div>
+        )}
+        <br />
+        <></>
         <select
           style={{ position: "absolute", right: "20px", margin: "10px" }}
           id="yearDropdown"
@@ -111,8 +169,6 @@ function Goal() {
           }}
         >
           update goal
-          {/* check goal.goalTractates.length if > 0 then have a dropdown with .tractate.name else just show the first one
-           */}
         </div>
       </div>
       {openGoal && (
@@ -122,6 +178,7 @@ function Goal() {
           setGoalEditOption={setGoalEditOption}
           tractates={tractates}
           goal={goal}
+          setUser={setUser}
         />
       )}
     </div>
