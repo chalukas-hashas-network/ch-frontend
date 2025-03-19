18d8eab4df2e534169b511aa81dd808d51e0ddf1
Benny Sufrin <bennysuf@gmail.com>
2025-03-18 23:46:51 -0400

    update styling, change inputs to MUI text field

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,10 +3,16 @@
 import { useState, useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { useUser } from "../utils/Context";
-import { Button } from "../utils/dataExports/muiExports";
+import {
+  Button,
+  CloseRoundedIcon,
+  TextField,
+  MenuItem,
+  Dialog,
+} from "../utils/dataExports/muiExports";
 import states from "../utils/dataExports/StatesExports";
 
-function Login({ setLoginOpen, userStatus, setUserStatus }) {
+function Login({ setLoginOpen, userStatus, setUserStatus, loginOpen }) {
   const location = useLocation();
   const { login, triggerLoading } = useUser();
   const navigate = useNavigate();
@@ -47,6 +53,7 @@ function Login({ setLoginOpen, userStatus, setUserStatus }) {
       return;
     }
     setSignupPopup(true);
+    // setLoginOpen(false);
   };
 
   const handleFormToggle = () => {
@@ -96,141 +103,221 @@ function Login({ setLoginOpen, userStatus, setUserStatus }) {
   };
 
   return (
-    <div className="popup-overlay">
-      <div className="popup-card">
-        <h5 onClick={() => setLoginOpen(false)}>X</h5>
-        <article className="input" style={{ marginTop: "10px" }}>
-          {userStatus === "Login" && (
-            <form
-              onSubmit={(e) => {
-                e.preventDefault();
-                handleSubmit("Login");
-              }}
-            >
-              <input
-                type="text"
-                placeholder="Username"
-                name="username"
-                value={userData.username}
-                onChange={handleChange}
-              />
-              <br />
-              <input
-                type="password"
-                placeholder="Password"
-                name="password"
-                value={userData.password}
-                onChange={handleChange}
-              />
-              <Button type="submit">Login</Button>
-            </form>
-          )}
-
-          {userStatus === "Signup" && (
-            <div>
+    <Dialog open={loginOpen} className="popup-overlay" style={{borderRadius: "20px"}}>
+      {!signupPopup && (
+        <div
+          className="popup-card"
+          style={{ position: "relative", height: "30em", width: "22em", borderRadius: "20px" }}
+        >
+          <Button
+            onClick={() => setLoginOpen(false)}
+            style={{ position: "absolute", top: "10px", right: "10px" }}
+          >
+            <CloseRoundedIcon style={{ color: "var(--orange)" }} />
+          </Button>
+          <article className="input" style={{ marginTop: "10px" }}>
+            {userStatus === "Login" && (
               <form
                 onSubmit={(e) => {
                   e.preventDefault();
-                  // handleSubmit("Register user");
-                  handleSignup();
+                  handleSubmit("Login");
                 }}
               >
-                <input
-                  type="email"
-                  name="email"
-                  placeholder="Email"
-                  value={userData.email}
+                <label>Login</label>
+                <TextField
+                  id="username"
+                  label="Username"
+                  variant="outlined"
+                  type="text"
+                  name="username"
+                  value={userData.username}
                   onChange={handleChange}
+                  style={{
+                    width: "100%",
+                    marginBottom: "1em",
+                    marginTop: "2em",
+                  }}
                 />
-                <br />
-                <input
+                <TextField
+                  id="password"
+                  label="Password"
+                  variant="outlined"
                   type="password"
-                  placeholder="Password"
                   name="password"
                   value={userData.password}
                   onChange={handleChange}
+                  style={{ width: "100%", marginBottom: "1em" }}
                 />
-                <br />
-                <input
-                  type="password"
-                  placeholder="Confirm Password"
-                  name="confirmPassword"
-                  value={userData.confirmPassword}
-                  onChange={handleChange}
-                />
-                <br />
-                <Button type="submit">Signup</Button>
+                <Button
+                  type="submit"
+                  variant="contained"
+                  style={{ backgroundColor: "var(--orange)", width: "100%" }}
+                >
+                  Login
+                </Button>
               </form>
-            </div>
-          )}
+            )}
 
-          <Button type="button" onClick={handleFormToggle}>
-            {userStatus === "Login"
-              ? "Create an account"
-              : "Already have an account?"}
-          </Button>
-        </article>
-      </div>
-      {signupPopup && (
-        <div className="popup-overlay">
-          <div className="popup-card">
-            <button onClick={() => setSignupPopup(false)}>X</button>
-            <form
-              onSubmit={(e) => {
-                e.preventDefault();
-                handleSubmit("Register user");
+            {userStatus === "Signup" && (
+              <div>
+                <form
+                  onSubmit={(e) => {
+                    e.preventDefault();
+                    // handleSubmit("Register user");
+                    handleSignup();
+                  }}
+                >
+                  <label>Signup</label>
+                  <TextField
+                    id="email"
+                    label="Email"
+                    variant="outlined"
+                    type="text"
+                    name="email"
+                    value={userData.email}
+                    onChange={handleChange}
+                    style={{
+                      width: "100%",
+                      marginBottom: "1em",
+                      marginTop: "2em",
+                    }}
+                  />
+                  <TextField
+                    id="password"
+                    label="Password"
+                    variant="outlined"
+                    type="password"
+                    name="password"
+                    value={userData.password}
+                    onChange={handleChange}
+                    style={{ width: "100%", marginBottom: "1em" }}
+                  />
+                  <TextField
+                    id="confirm-password"
+                    label="Confirm Password"
+                    variant="outlined"
+                    type="password"
+                    name="confirmPassword"
+                    value={userData.confirmPassword}
+                    onChange={handleChange}
+                    style={{ width: "100%", marginBottom: "1em" }}
+                  />
+                  <Button
+                    type="submit"
+                    variant="contained"
+                    style={{ backgroundColor: "var(--orange)", width: "100%" }}
+                  >
+                    Signup
+                  </Button>
+                </form>
+              </div>
+            )}
+
+            <Button
+              type="button"
+              onClick={handleFormToggle}
+              style={{
+                color: "var(--orange)",
+                position: "absolute",
+                bottom: "10px",
+                right: "10px",
               }}
             >
-              <h1>Register</h1>
-              <input
-                type="text"
-                name="username"
-                placeholder="Username"
-                value={userData.username}
-                onChange={handleChange}
-              />
-              <input
-                type="text"
-                name="first_name"
-                placeholder="First Name"
-                value={userData.first_name}
-                onChange={handleChange}
-              />
-              <br />
-              <input
-                type="text"
-                name="last_name"
-                placeholder="Last Name"
-                value={userData.last_name}
-                onChange={handleChange}
-              />
-              <br />
-              <input
-                type="tel"
-                name="phone_number"
-                placeholder="Phone Number"
-                value={userData.phone_number}
-                onChange={handleChange}
-              />
-              <br />
-              <select
-                name="location"
-                value={userData.location}
-                onChange={handleChange}
-              >
-                <option value="">Select state</option>
-                {states.map((state, index) => (
-                  <option key={index} value={state}>
-                    {state}
-                  </option>
-                ))}
-              </select>
-              <Button type="submit">Create Account</Button>
-            </form>
-          </div>
+              {userStatus === "Login"
+                ? "Create an account"
+                : "Already have an account?"}
+            </Button>
+          </article>
         </div>
       )}
-    </div>
+      {signupPopup && (
+        <div
+          className="popup-card"
+          style={{ position: "relative", height: "30em", width: "22em" }}
+        >
+          <Button
+            onClick={() => {
+              setSignupPopup(false);
+              setLoginOpen(true);
+            }}
+            style={{ position: "absolute", top: "10px", right: "10px" }}
+          >
+            <CloseRoundedIcon style={{ color: "var(--orange)" }} />
+          </Button>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              handleSubmit("Register user");
+            }}
+          >
+            <label>Register</label>
+            <TextField
+              id="username"
+              label="Username"
+              variant="outlined"
+              type="text"
+              name="username"
+              value={userData.username}
+              onChange={handleChange}
+              style={{ width: "100%", marginBottom: "1em", marginTop: "2em" }}
+            />
+            <TextField
+              id="first-name"
+              label="First Name"
+              variant="outlined"
+              type="text"
+              name="first_name"
+              value={userData.first_name}
+              onChange={handleChange}
+              style={{ width: "100%", marginBottom: "1em" }}
+            />
+            <TextField
+              id="last-name"
+              label="Last Name"
+              variant="outlined"
+              type="text"
+              name="last_name"
+              value={userData.last_name}
+              onChange={handleChange}
+              style={{ width: "100%", marginBottom: "1em" }}
+            />
+            <TextField
+              id="phone-number"
+              label="Phone Number"
+              variant="outlined"
+              type="tel"
+              name="phone_number"
+              value={userData.phone_number}
+              onChange={handleChange}
+              style={{ width: "100%", marginBottom: "1em" }}
+            />
+            <TextField
+              id="outlined-select-state"
+              select
+              label="State"
+              name="location"
+              value={userData.location}
+              onChange={handleChange}
+              defaultValue="Select state"
+              style={{ width: "100%", marginBottom: "1em" }}
+            >
+              {states.map((state, index) => (
+                <MenuItem key={index} value={state}>
+                  {state}
+                </MenuItem>
+              ))}
+            </TextField>
+            <Button
+              type="submit"
+              variant="contained"
+              style={{ backgroundColor: "var(--orange)", width: "100%" }}
+            >
+              Create Account
+            </Button>
+          </form>
+        </div>
+      )}
+    </Dialog>
   );
 }
 
