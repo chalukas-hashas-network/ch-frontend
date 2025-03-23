041cdaa47fc6279148832c27e0f28aa9ee7d4de4
Benny Sufrin <bennysuf@gmail.com>
2025-03-23 17:35:31 -0400

    change inputs to text fields

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -3,11 +3,17 @@ import { useState } from "react";
 import { updateUser } from "../utils/API/UserAPI";
 import { useUser } from "../utils/Context";
 import states from "../utils/dataExports/StatesExports";
+import { Button, TextField, MenuItem } from "../utils/dataExports/muiExports";
 
 function Settings() {
   const { user } = useUser();
   const { first_name, last_name, username, email, phone_number, location } =
     user;
+
+    /*
+     required:
+     username, email, 
+     */
 
   const [userData, setUserData] = useState({
     username: username,
@@ -46,7 +52,7 @@ function Settings() {
     }
     try {
       await updateUser(updatedFields);
-      alert("Profile updated successfully!");
+      // alert("Profile updated successfully!");
     } catch (err) {
       console.error(err);
       alert("Failed to update profile. Please try again later.");
@@ -54,84 +60,114 @@ function Settings() {
   };
 
   return (
-    <div style={{ color: "white", paddingTop: "100px" }}>
+    <div style={{ color: "white", paddingTop: "100px"}}>
       <Link style={{ textDecoration: "none", color: "white" }} to="/home">
         {"< Back"}
       </Link>
-      <div style={{ marginTop: "20px", display: "flex", flexDirection: "column", alignItems: "center" }}>
-        <form onSubmit={submitUpdatedProfile}>
-          <label>
-            Username:{" "}
-            <input
-              type="text"
-              name="username"
-              placeholder="Username"
-              value={userData.username}
-              onChange={handleDataChange}
-            />
-          </label>
-          <br />
-          <label>
-            First Name:{" "}
-            <input
-              type="text"
-              name="first_name"
-              placeholder="First Name"
-              value={userData.first_name}
-              onChange={handleDataChange}
-            />
-          </label>
-          <br />
-          <label>
-            Last Name:{" "}
-            <input
-              type="text"
-              name="last_name"
-              placeholder="Last Name"
-              value={userData.last_name}
-              onChange={handleDataChange}
-            />
-          </label>
-          <br />
-          <label>
-            Email:{" "}
-            <input
-              type="text"
-              name="email"
-              placeholder="Email"
-              value={userData.email}
-              onChange={handleDataChange}
-            />
-          </label>
-          <br />
-          <label>
-            Phone Number:{" "}
-            <input
-              type="text"
-              name="phone_number"
-              placeholder="Phone Number"
-              value={userData.phone_number}
-              onChange={handleDataChange}
-            />
-          </label>
-          <br />
-          <label>
-            State:{" "}
-            <select
-              value={capitalizeWord(userData.location)}
-              name="location"
-              onChange={handleDataChange}
-            >
-              <option value="">Select state</option>
-              {states.map((state, index) => (
-                <option key={index} value={state}>
-                  {state}
-                </option>
-              ))}
-            </select>
-          </label>
-          <br />
-          <input type="submit" value="Update Profile" />
+      <div
+        style={{
+          marginTop: "20px",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <form onSubmit={submitUpdatedProfile} style={{color: "white"}}>
+          <TextField
+            id="username"
+            label="Username"
+            variant="outlined"
+            type="text"
+            name="username"
+            value={userData.username}
+            onChange={handleDataChange}
+            sx={{ width: "90%", marginBottom: "1em", marginTop: "2em", 
+              //  "& .MuiOutlinedInput-root": {
+              // backgroundColor: "white",
+              // "&.Mui-focused fieldset": {
+              //   borderColor: "black"
+              // },
+               "& input": {
+                color: "white"
+              // }
+            }}}
+          />
+          <TextField
+            id="first-name"
+            label="First Name"
+            variant="outlined"
+            type="text"
+            name="first_name"
+            value={userData.first_name}
+            onChange={handleDataChange}
+            sx={{ width: "90%", marginBottom: "1em", "& input": {
+                color: "white"
+              } }}
+          />
+          <TextField
+            id="last-name"
+            label="Last Name"
+            variant="outlined"
+            type="text"
+            name="last_name"
+            value={userData.last_name}
+            onChange={handleDataChange}
+              sx={{ width: "90%", marginBottom: "1em", "& input": {
+                color: "white"
+              } }}
+          />
+          <TextField
+            id="email"
+            label="Email"
+            variant="outlined"
+            type="text"
+            name="email"
+            value={userData.email}
+            onChange={handleDataChange}
+              sx={{ width: "90%", marginBottom: "1em", "& input": {
+                color: "white"
+              } }}
+          />
+          <TextField
+            id="phone-number"
+            label="Phone Number"
+            variant="outlined"
+            type="tel"
+            name="phone_number"
+            value={userData.phone_number}
+            onChange={handleDataChange}
+              sx={{ width: "90%", marginBottom: "1em", "& input": {
+                color: "white"
+              } }}
+          />
+          <TextField
+            id="outlined-select-state"
+            select
+            label="State"
+            name="location"
+            value={capitalizeWord(userData.location)}
+            onChange={handleDataChange}
+            defaultValue="Select state"
+            sx={{ width: "90%", marginBottom: "1em", "& input": {
+                color: "white"
+              } }}
+          >
+            <MenuItem value="Select state">Select state</MenuItem>
+            {states.map((state, index) => (
+              <MenuItem key={index} value={state}>
+                {state}
+              </MenuItem>
+            ))}
+          </TextField>
+          <Button
+            type="submit"
+            variant="contained"
+            sx={{ backgroundColor: "var(--orange)", width: "90%", "& input": {
+                color: "white"
+              } }}
+          >
+            Update Profile
+          </Button>
         </form>
       </div>
     </div>
