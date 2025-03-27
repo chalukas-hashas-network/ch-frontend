72587671547516f6f52237b6c6fe82f9245435b5
Benny Sufrin <bennysuf@gmail.com>
2025-03-26 22:20:37 -0400

    make pregress bar dynamic and update styling

--- src/pages/Goal.js
+++ src/pages/Goal.js
@@ -3,7 +3,15 @@ import { useEffect, useState } from "react";
 import GoalPopup from "../components/GoalPopup";
 import { useUser } from "../utils/Context";
 import { createAnnualGoal, getAllTractates } from "../utils/API/GoalAPI";
-import { Box, LinearProgress, Typography } from "../utils/dataExports/muiExports";
+import {
+  Box,
+  LinearProgress,
+  Typography,
+  ArrowBackIcon,
+  Paper,
+  MenuItem,
+  TextField,
+} from "../utils/dataExports/muiExports";
 
 /*
 TODO: 
@@ -12,25 +20,29 @@ see if theres a goal with goal.year.includes(current.year) (may have to change y
 if not, on submit, create new goal then create new goalTractate
 or have start goal for {current.year} and onclick create goal and open popup for "create goal"
 TODO: build out progress bar, allow for user to delete goal, finish year logic, update user state upon any edits, close popup on submit, fix page dropdown after submit (goes to default)
+TODO: if goal is empty, show "no goal"
 */
 
 function Goal() {
   const [openGoal, setOpenGoal] = useState(false);
   const [goalEditOption, setGoalEditOption] = useState("");
   const [tractates, setTractates] = useState([]);
-  const [selectedTractate, setSelectedTractate] = useState({
-    tractate: "total",
-    pages_completed: 0,
-    pages_selected: 0,
-  });
   const { user, setUser } = useUser();
   const { goal } = user;
+
+  const [selectedTractate, setSelectedTractate] = useState({
+    tractate: "total",
+    percentage_completed: goal.user_percentage_completed || 0,
+    pages_completed: goal.user_total_completed_pages || 0,
+    pages_selected: goal.user_total_selected_pages || 0,
+  });
 
   useEffect(
     function getTractates() {
       const fetchTractates = async () => {
         try {
           const tractates = await getAllTractates();
+          //TODO:  if tractates are filled dont populate
           setTractates(tractates);
           console.log("tractates", tractates);
         } catch (e) {
@@ -46,21 +58,22 @@ function Goal() {
 
   const currentYear = new Date().getFullYear();
 
-  const jewishYear = currentYear + 3760; // Get the jewish year
-  const [selectedYear, setSelectedYear] = useState(jewishYear);
-  // Create an array of years from the current Jewish year down to 5783
-  const years = [];
-  // ! change year to start from 5785
-  for (let year = jewishYear; year >= 5783; year--) {
-    years.push(year);
-  }
-  const handleYearChange = (event) => {
-    setSelectedYear(event.target.value);
-  };
+  // const jewishYear = currentYear + 3760; // Get the jewish year
+  // const [selectedYear, setSelectedYear] = useState(jewishYear);
+  // // Create an array of years from the current Jewish year down to 5783
+  // const years = [];
+  // // ! change year to start from 5785
+  // for (let year = jewishYear; year >= 5783; year--) {
+  //   years.push(year);
+  // }
+
+  // const handleYearChange = (event) => {
+  //   setSelectedYear(event.target.value);
+  // };
 
   const createGoalForCurrentYear = async () => {
     try {
-      await createAnnualGoal(user.id, `${currentYear}-${currentYear + 1}`);
+      await createAnnualGoal(user.id, `${currentYear - 1}-${currentYear}`);
       alert("created new goal for " + currentYear);
       setGoalEditOption("create-goal");
       setOpenGoal(true);
@@ -75,78 +88,161 @@ function Goal() {
       (tractate) => tractate.tractate === event.target.value
     );
     if (selectedGoal) {
+      const { tractate, tractate_pages_completed, tractate_pages_selected } =
+        selectedGoal;
+
+      const percentageCompleted =
+        (tractate_pages_completed / tractate_pages_selected) * 100;
+
       setSelectedTractate({
-        tractate: selectedGoal.tractate,
-        pages_completed: selectedGoal.tractate_pages_completed,
-        pages_selected: selectedGoal.tractate_pages_selected,
+        tractate: tractate,
+        percentage_completed: percentageCompleted.toFixed(2),
+        pages_completed: tractate_pages_completed,
+        pages_selected: tractate_pages_selected,
       });
     } else {
       setSelectedTractate({
         tractate: "total",
-        pages_completed: goal?.user_total_completed_pages,
-        pages_selected: goal?.user_total_selected_pages,
+        percentage_completed: goal?.user_percentage_completed || 0,
+        pages_completed: goal?.user_total_completed_pages || 0,
+        pages_selected: goal?.user_total_selected_pages || 0,
       });
     }
   };
 
   return (
-    <div style={{ color: "white", paddingTop: "100px" }}>
-      <Link style={{ textDecoration: "none", color: "white" }} to="/home">
-        {"< Back"}
+    <div style={{ color: "black", paddingTop: "100px" }}>
+      <Link
+        style={{ textDecoration: "none", color: "black", marginLeft: "2em" }}
+        to="/home"
+      >
+        <ArrowBackIcon />
       </Link>
-      <div className="card" style={{ height: "150px" }}>
-        <>User Progress Bar </>
-        <Box sx={{ display: "flex", alignItems: "center", margin: "10px" }}>
-          <Box sx={{ width: "100%", mr: 1 }}>
-            <LinearProgress
-              variant="determinate"
-              value={goal.user_percentage_completed}
-              style={{ margin: "10px", height: "20px" }}
-            />
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Paper
+          elevation={3}
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            width: "90%",
+            marginTop: "20px",
+          }}
+        >
+          <Typography sx={{ marginTop: ".5em" }}>User Progress Bar </Typography>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              margin: "10px",
+              width: "90%",
+            }}
+          >
+            <Box sx={{ width: "100%", mr: 1 }}>
+              <LinearProgress
+                variant="determinate"
+                value={selectedTractate.percentage_completed}
+                sx={{
+                  margin: "10px",
+                  height: "20px",
+                  backgroundColor: "var(--orange-light)",
+                  "& .MuiLinearProgress-bar": {
+                    backgroundColor: "var(--orange)",
+                  },
+                }}
+              />
+            </Box>
+            <Box sx={{ minWidth: 35 }}>
+              <Typography
+                variant="body2"
+                color="text.secondary"
+                style={{ color: "black" }}
+              >{`${selectedTractate.percentage_completed}%`}</Typography>
+            </Box>
           </Box>
-          <Box sx={{ minWidth: 35 }}>
-            <Typography
-              variant="body2"
-              color="text.secondary"
-              style={{ color: "white" }}
-            >{`${goal.user_percentage_completed}%`}</Typography>
+          <Box
+            sx={{
+              alignItems: "center",
+              marginBottom: "1em",
+              display: "flex",
+              justifyContent: "center",
+              width: "100%",
+              flexDirection: { xs: "column", md: "row" },
+            }}
+          >
+            {user?.goal?.goal_tractates?.length > 1 ? (
+              <>
+                <TextField
+                  select
+                  autoWidth
+                  value={selectedTractate.tractate}
+                  onChange={handleTractateChange}
+                  id="grouped-select"
+                  label="Tractate"
+                  sx={{
+                    marginRight: "1em",
+                    width: "15em",
+                    "& .MuiOutlinedInput-root": {
+                      color: "black",
+                      "& fieldset": {
+                        borderColor: "black",
+                      },
+                      "&:hover fieldset": {
+                        borderColor: "var(--orange-light)",
+                      },
+                      "&.Mui-focused fieldset": {
+                        borderColor: "var(--orange)",
+                      },
+                    },
+                    "& .MuiInputLabel-root": {
+                      color: "black",
+                    },
+                  }}
+                >
+                  <MenuItem key="total" value="total">
+                    <em>Total</em>
+                  </MenuItem>
+                  {user.goal.goal_tractates.map((tractate) => (
+                    <MenuItem key={tractate.id} value={tractate.tractate}>
+                      {tractate.tractate}
+                    </MenuItem>
+                  ))}
+                </TextField>
+                <br />
+                <Typography>
+                  Pages completed: {selectedTractate.pages_completed} /{" "}
+                  {selectedTractate.pages_selected}
+                  {/* {selectedTractate?.tractate === "total"
+                    ? goal?.user_total_completed_pages
+                    : selectedTractate.pages_completed}{" "}
+                  /{" "}
+                  {selectedTractate?.tractate === "total"
+                    ? goal?.user_total_selected_pages
+                    : selectedTractate.pages_selected} */}
+                </Typography>
+              </>
+            ) : (
+              <>
+                <span style={{ marginLeft: "10px" }}>
+                  {user?.goal?.goal_tractates?.[0]?.tractate ||
+                    "No tractate selected"}
+                </span>
+                <br />
+                <Typography>
+                  Pages completed: {goal?.user_total_completed_pages} /{" "}
+                  {goal?.user_total_selected_pages}
+                </Typography>
+              </>
+            )}
           </Box>
-        </Box>
-        {user?.goal?.goal_tractates?.length > 1 ? (
-          <div>
-            <select
-              style={{ marginLeft: "10px" }}
-              onChange={handleTractateChange}
-            >
-              <option value="total">Total</option>
-              {user.goal.goal_tractates.map((tractate) => (
-                <option key={tractate.id} value={tractate.tractate}>
-                  {tractate.tractate}
-                </option>
-              ))}
-            </select>
-            <br />
-            Pages completed:{" "}
-            {selectedTractate?.tractate === "total"
-              ? goal?.user_total_completed_pages
-              : selectedTractate.pages_completed}{" "}
-            /{" "}
-            {selectedTractate?.tractate === "total"
-              ? goal?.user_total_selected_pages
-              : selectedTractate.pages_selected}
-          </div>
-        ) : (
-          <div>
-            <span style={{ marginLeft: "10px" }}>
-              {user?.goal?.goal_tractates?.[0]?.tractate ||
-                "No tractate selected"}
-            </span>
-            <br />
-            Pages complete: {goal?.user_total_completed_pages} /{" "}
-            {goal?.user_total_selected_pages}
-          </div>
-        )}
-        <select
+          {/* <select
           style={{ position: "absolute", right: "20px", margin: "10px" }}
           id="yearDropdown"
           value={selectedYear}
@@ -157,44 +253,57 @@ function Goal() {
               {year}
             </option>
           ))}
-        </select>
-      </div>
-      <div>
-        {goal.year.includes(currentYear) ? (
-          <div
-            className="card"
-            onClick={() => {
-              setOpenGoal(true);
-              setGoalEditOption("create-goal");
-            }}
-          >
-            create goal
-          </div>
-        ) : (
-          <div className="card" onClick={() => createGoalForCurrentYear()}>
-            create goal
-          </div>
-        )}
-        <div
-          className="card"
-          onClick={() => {
-            setOpenGoal(true);
-            setGoalEditOption("update-goal");
+        </select> */}
+        </Paper>
+        <Box
+          sx={{
+            width: { xs: "50%", md: "25%" },
+            height: "300px",
+            marginTop: "2em",
           }}
         >
-          update goal
-        </div>
+          {goal?.year?.includes(currentYear) ? (
+            <div
+              className="card"
+              style={{ height: "3em" }}
+              onClick={() => {
+                setOpenGoal(true);
+                setGoalEditOption("create-goal");
+              }}
+            >
+              <Typography>create goal</Typography>
+            </div>
+          ) : (
+            <div
+              className="card"
+              style={{ height: "3em" }}
+              onClick={() => createGoalForCurrentYear()}
+            >
+              <Typography>create goal</Typography>
+            </div>
+          )}
+          <div
+            className="card"
+            style={{ height: "3em", marginTop: "1em" }}
+            onClick={() => {
+              setOpenGoal(true);
+              setGoalEditOption("update-goal");
+            }}
+          >
+            <Typography>update goal</Typography>
+          </div>
+        </Box>
+        {openGoal && (
+          <GoalPopup
+            setOpenGoal={setOpenGoal}
+            goalEditOption={goalEditOption}
+            setGoalEditOption={setGoalEditOption}
+            tractates={tractates}
+            goal={goal}
+            setUser={setUser}
+          />
+        )}
       </div>
-      {openGoal && (
-        <GoalPopup
-          setOpenGoal={setOpenGoal}
-          goalEditOption={goalEditOption}
-          setGoalEditOption={setGoalEditOption}
-          tractates={tractates}
-          goal={goal}
-          setUser={setUser}
-        />
-      )}
     </div>
   );
 }
