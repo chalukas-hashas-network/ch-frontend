468c932113cf424a6650b63563cb0afe3d946b63
Benny Sufrin <bennysuf@gmail.com>
2025-03-31 22:28:09 -0400

    change elements to MUI elements, add paper and other styling, change progress bar to slider, update logic

--- src/components/CommunityMembersList.js
+++ src/components/CommunityMembersList.js
@@ -1,13 +1,8 @@
 import {
-  Box,
-  LinearProgress,
   Typography,
-  List,
-  ListItemButton,
-  ListItemAvatar,
-  ListItemText,
-  Divider,
-  Avatar,
+  Paper,
+  Slider,
+  Card,
 } from "../utils/dataExports/muiExports";
 import { useEffect, useState } from "react";
 import { getCommunities } from "../utils/API/CommunityAPI";
@@ -16,7 +11,7 @@ function CommunityMembersList({ selectedCommunity }) {
   const [communityData, setCommunityData] = useState({
     community_goal: {
       community_total_completed_pages: 0,
-      community_total_selected_pages: 0,
+      community_total_selected_pages: 1,
     },
     members: [],
     name: "",
@@ -32,10 +27,9 @@ function CommunityMembersList({ selectedCommunity }) {
 
       const goal = community_goal?.[0] || {
         community_total_completed_pages: 0,
-        community_total_selected_pages: 0,
+        community_total_selected_pages: 1,
       };
 
-      console.log("goal", goal);
       setCommunityData({
         community_goal: {
           community_total_completed_pages: goal.community_total_completed_pages,
@@ -51,90 +45,157 @@ function CommunityMembersList({ selectedCommunity }) {
     community_goal;
 
   const percentageCompleted =
-    (community_total_completed_pages / community_total_selected_pages) * 100;
-
-  console.log("selectedCommunity", selectedCommunity);
+    (community_total_completed_pages / community_total_selected_pages) * 100 ||
+    0;
 
   return (
-    <div>
-      <h1>{name} Members List</h1>
-      <div className="card" style={{ height: "150px" }}>
-        <div>{name} Progress Bar </div>
-        <Box
-          className="progressBarContainer"
-          sx={{ display: "flex", alignItems: "center", margin: "10px" }}
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <Paper
+        elevation={0}
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "90%",
+          marginTop: "20px",
+          border: "2px solid lightgrey",
+          borderRadius: "16px",
+        }}
+      >
+        <Typography
+          variant="h6"
+          sx={{ paddingTop: "1em", color: "var(--brown)" }}
         >
-          <Box className="progressBar" sx={{ width: "100%", mr: 1 }}>
-            <LinearProgress
-              variant="determinate"
-              value={percentageCompleted}
-              sx={{
-                margin: "10px",
-                height: "20px",
-                backgroundColor: "var(--orange-light)",
-                "& .MuiLinearProgress-bar": {
-                  backgroundColor: "var(--orange)",
-                },
-              }}
-            />
-          </Box>
-          <Box className="percentageNum" sx={{ minWidth: 35 }}>
-            <Typography
-              variant="body2"
-              color="text.secondary"
-              style={{ color: "black" }}
-            >
-              {percentageCompleted
-                ? `${percentageCompleted.toFixed(2)}%`
-                : "0%"}
-            </Typography>
-          </Box>
-        </Box>
-        <div>
-          Pages complete: {community_total_completed_pages} /{" "}
-          {community_total_selected_pages}
-        </div>
-      </div>
+          {name} Community
+        </Typography>
+        <Typography variant="body2" sx={{ color: "var(--orange)" }}>
+          {members.length > 0
+            ? `${members.length} Community Members`
+            : "No Members Yet"}{" "}
+        </Typography>
+        <Slider
+          disabled
+          value={percentageCompleted}
+          aria-label="Disabled slider"
+          sx={{
+            width: "90%",
+            "& .MuiSlider-thumb": {
+              color: "var(--light-blue)",
+              height: "12px",
+              width: "12px",
+            },
+            "& .MuiSlider-track": {
+              color: "var(--light-blue)",
+            },
+            "& .MuiSlider-rail": {
+              color: "var(--light-grey)",
+            },
+          }}
+        />
+        <Typography
+          variant="body2"
+          color="text.secondary"
+          sx={{ color: "var(--light-blue)", marginBottom: "1em" }}
+        >
+          {percentageCompleted ? `${percentageCompleted.toFixed(2)}%` : "0%"}
+        </Typography>
+      </Paper>
       <br />
-      <div
-        style={{
+      <Paper
+        className="membersContainer"
+        elevation={0}
+        sx={{
           alignItems: "center",
           display: "flex",
           alignContent: "center",
           flexDirection: "column",
+          width: "90%",
+          marginTop: "20px",
+          backgroundColor: "var(--light-grey)",
+          height: "300px",
+          borderRadius: "16px",
         }}
       >
-        {members?.length > 0 ? (
-          members?.map((member) => (
-            <List
-              key={member.id}
-              sx={{
-                width: "100%",
-                maxWidth: 360,
-                bgcolor: "background.paper",
-                color: "black",
-              }}
-              onClick={() => {
-                console.log("member", member);
-              }}
-            >
-              <ListItemButton alignItems="flex-start">
-                <ListItemAvatar>
-                  <Avatar
-                    alt={member.first_name + " " + member.last_name}
-                    src={member?.image}
-                  />
-                </ListItemAvatar>
-                <ListItemText
-                  primary={member.first_name + " " + member.last_name}
-                />
-              </ListItemButton>
-              <Divider variant="inset" component="li" />
-            </List>
-          ))
-        ) : (
-          <p>No members for this community</p>
-        )}
+        <Paper
+          elevation={0}
+          className="listOfMembers"
+          sx={{
+            backgroundColor: "white",
+            width: "90%",
+            height: "80%",
+            margin: "2em",
+            borderRadius: "16px",
+          }}
+        >
+          {members?.length > 0 ? (
+            members?.map((member) => (
+              <Card
+                sx={{
+                  backgroundColor: "var(--light-grey)",
+                  width: "90%",
+                  margin: "1em",
+                  borderRadius: "25px",
+                }}
+                onClick={() => console.log("member", member)}
+              >
+                <Typography variant="h6" sx={{ marginLeft: "1.5em" }}>
+                  {member.first_name + " " + member.last_name}
+                </Typography>
+                <Typography variant="caption" sx={{ marginLeft: "2em" }}>
+                  With {member.community}
+                </Typography>
+              </Card>
+            ))
+          ) : (
+            <p>No members for this community</p>
+          )}
+        </Paper>
+      </Paper>
+      <div>
+        <Paper
+          elevation={0}
+          className="outerContainer"
+          sx={{
+            backgroundColor: "var(--light-grey)",
+            width: "150px",
+            height: "100px",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            marginTop: "1em",
+            borderRadius: "16px",
+          }}
+        >
+          <Paper
+            elevation={0}
+            className="innerContainer"
+            sx={{
+              backgroundColor: "white",
+              width: "90%",
+              height: "90%",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              flexDirection: "column",
+              borderRadius: "16px",
+            }}
+          >
+            <Typography sx={{ color: "var(--light-blue)", fontSize: "1em" }}>
+              {community_total_completed_pages} /{" "}
+              {community_total_selected_pages}
+            </Typography>
+            <Typography sx={{ color: "black", fontSize: ".8em" }}>
+              Pages Learned
+            </Typography>
+          </Paper>
+        </Paper>
       </div>
     </div>
   );
