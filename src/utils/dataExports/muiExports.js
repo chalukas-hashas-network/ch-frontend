7cb526fd3fc23b6ddd2a32d59688648f3504b8fe
Benny Sufrin <bennysuf@gmail.com>
2025-04-03 10:04:26 -0700

    yup.. more imports

--- src/utils/dataExports/muiExports.js
+++ src/utils/dataExports/muiExports.js
@@ -1,4 +1,5 @@
 import Autocomplete from "@mui/material/Autocomplete";
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import Avatar from '@mui/material/Avatar';
 import Box from '@mui/material/Box';
 import Button from "@mui/material/Button";
@@ -11,19 +12,27 @@ import Container from "@mui/material/Container";
 import DeleteIcon from '@mui/icons-material/Delete';
 import Dialog from "@mui/material/Dialog";
 import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
 import EastRoundedIcon from "@mui/icons-material/EastRounded";
 import Grid from "@mui/material/Grid2";
 import IconButton from "@mui/material/IconButton";
 import LinearProgress from "@mui/material/LinearProgress";
 import List from "@mui/material/List";
+import ListItem from '@mui/material/ListItem';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemText from "@mui/material/ListItemText";
 import Menu from "@mui/material/Menu";
 import MenuIcon from "@mui/icons-material/Menu";
 import MenuItem from "@mui/material/MenuItem";
+import NoteAddIcon from "@mui/icons-material/NoteAdd";
 import Paper from "@mui/material/Paper";
 import PersonAddAltRoundedIcon from '@mui/icons-material/PersonAddAltRounded';
+import Select from '@mui/material/Select';
+import Slider from '@mui/material/Slider';
+import SpeedDial from "@mui/material/SpeedDial";
+import SpeedDialAction from "@mui/material/SpeedDialAction"
+import SpeedDialIcon from "@mui/material/SpeedDialIcon";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
@@ -36,9 +45,11 @@ import TextField from "@mui/material/TextField";
 import ToggleButton from "@mui/material/ToggleButton";
 import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
 import Typography from "@mui/material/Typography";
+import UpgradeIcon from "@mui/icons-material/Upgrade";
 
 export {
   Autocomplete,
+  ArrowBackIcon,
   Avatar,
   Box,
   Button,
@@ -51,19 +62,27 @@ export {
   DeleteIcon,
   Dialog,
   Divider,
+  Drawer,
   EastRoundedIcon,
   Grid,
   IconButton,
   LinearProgress,
   List,
+  ListItem,
   ListItemAvatar,
   ListItemButton,
   ListItemText,
   Menu,
   MenuIcon,
   MenuItem,
+  NoteAddIcon,
   Paper,
   PersonAddAltRoundedIcon,
+  Select,
+  Slider,
+  SpeedDial,
+  SpeedDialAction,
+  SpeedDialIcon,
   Table,
   TableBody,
   TableCell,
@@ -76,4 +95,5 @@ export {
   ToggleButton,
   ToggleButtonGroup,
   Typography,
+  UpgradeIcon,
 };
