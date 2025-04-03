90cdebb2da493a161860855cc0dfb143261afeb5
Benny Sufrin <bennysuf@gmail.com>
2025-04-03 10:07:31 -0700

    Update fetch URLs, enhanced error handling comments, and adjustments to request headers.

--- src/utils/API/UserAPI.js
+++ src/utils/API/UserAPI.js
@@ -16,7 +16,8 @@ export const getUser = async () => {
 
   try {
     const response = await fetch(
-      API_URL + "/me/?include_community=true&include_goal=true&include_goal_tractates=true",
+      API_URL +
+        "/me/?include_community=true&include_goal=true&include_goal_tractates=true",
       {
         method: "GET",
         headers: headers,
@@ -41,12 +42,14 @@ export const findUserById = async (userId) => {
   if (token) {
     headers["Authorization"] = `Bearer ${token}`;
   }
-
   try {
-    const response = await fetch(API_URL + `/user/${userId}/`, {
-      method: "GET",
-      headers: headers,
-    });
+    const response = await fetch(
+      API_URL + `/user/${userId}/?include_goal=true`,
+      {
+        method: "GET",
+        headers: headers,
+      }
+    );
     if (!response.ok) {
       throw new Error(`Error: ${response.statusText}`);
     }
@@ -67,18 +70,18 @@ export const queryUsers = async (filters = {}) => {
   }
 
   let url = `${API_URL}/admin/users/`;
-  
+
   // Dynamically add query parameters if there are any filters
   const queryParams = [];
   for (const [key, value] of Object.entries(filters)) {
     queryParams.push(`by_${key}=${value}`);
   }
-  
+
   // If there are query parameters, join them with '&' and append to URL
   if (queryParams.length > 0) {
     url += `?${queryParams.join("&")}`;
   }
-  
+
   try {
     const response = await fetch(url, {
       method: "GET",
@@ -98,13 +101,18 @@ export const queryUsers = async (filters = {}) => {
 };
 
 export const createUser = async (body) => {
+  // TODO: updated context isAuth = true
+  // cant call user context from here, return success message and update context where fetch is called
   console.log("hit create user request");
+  // body["community_id"] = 20;
   // hard code in body is_community_admin = false
   // body comes as an object, would need to add ^ to object before putting in body
   try {
     const response = await fetch(API_URL + "/user/register/", {
       method: "POST",
-      // headers,
+      headers: {
+        "Content-Type": "application/json",
+      },
       body: JSON.stringify(body),
     });
     if (!response.ok) {
@@ -115,6 +123,18 @@ export const createUser = async (body) => {
     // setUser(data);  // Set the fetched user data
   } catch (err) {
     // setError(err.message);  // Set the error message
+    // TODO: error messages if arent unique
+    //   {
+    //     "username": [
+    //         "A user with that username already exists."
+    //     ],
+    //     "email": [
+    //         "Email is already taken."
+    //     ],
+    //     "phone_number": [
+    //         "Phone number is already taken."
+    //     ]
+    // }
   } finally {
     // setLoading(false);  // Set loading to false once the fetch is complete
   }
@@ -128,7 +148,7 @@ export const updateUser = async (body) => {
   }
 
   try {
-    const response = await fetch(API_URL + "/me/update/", {
+    const response = await fetch(API_URL + "/me/", {
       method: "PATCH",
       headers,
       body: JSON.stringify(body),
@@ -139,6 +159,9 @@ export const updateUser = async (body) => {
 
     return await response.json();
     // setUser(data);  // Set the fetched user data
+    // error handling:
+    // {"email":["Email is already taken."]}
+    // {"email":["Enter a valid email address."]}
   } catch (err) {
     // setError(err.message);  // Set the error message
   } finally {
