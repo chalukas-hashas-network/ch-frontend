1eef1be014db66fe11b8bf1ef4677573ff5419c4
Benny Sufrin <bennysuf@gmail.com>
2025-04-22 13:50:17 -0400

    Integrate login functionality into App component, manage login state visibility based on route changes

--- src/components/App.js
+++ src/components/App.js
@@ -2,22 +2,33 @@
 // TODO: input validations, error handling, update user state upon any edits
 
 import Routing from "../utils/Routing";
-import { useUser } from "../utils/Context";
+import { useUser, useLogin } from "../utils/Context";
+import { useLocation } from "react-router-dom";
+import Login from "./Login";
 import Loading from "./Loading";
 import { useEffect } from "react";
 import Nav from "./Nav";
 
 function App() {
+  const location = useLocation();
   const { isLoading, triggerLoading } = useUser();
+  const { loginOpen, setLoginOpen } = useLogin();
 
   useEffect(() => {
     triggerLoading();
   }, []);
 
+  useEffect(
+    function closeLoginIfPageChange(){
+      setLoginOpen(false)
+    }, [location.pathname]
+  )
+
   return (
     <div className="App">
       {!isLoading && <Nav />}
       {isLoading ? <Loading /> : <Routing />}
+      {loginOpen && <Login />}
     </div>
   );
 }
