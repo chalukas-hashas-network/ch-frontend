559711119454e5f5c0131bd25f9aa01bcf40c567
Benny Sufrin <bennysuf@gmail.com>
2025-04-24 15:06:47 -0400

    refactor Login component: integrate community and tractate fetching, enhance user data handling, and streamline signup process with new state management

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,33 +2,38 @@
 // need to check if user is already signed up
 import { useState, useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
-import { useUser } from "../utils/Context";
+import { useUser, useLogin } from "../utils/Context";
 import { createUser } from "../utils/API/UserAPI";
-import {
-  Button,
-  CloseRoundedIcon,
-  TextField,
-  MenuItem,
-  Dialog,
-  Card,
-} from "../utils/dataExports/muiExports";
-import states from "../utils/dataExports/StatesExports";
+import { getCommunities } from "../utils/API/CommunityAPI";
+import { getAllTractates } from "../utils/API/GoalAPI";
+import { Button, CloseRoundedIcon } from "../utils/dataExports/muiExports";
+import LoginPopup from "./LoginPopup";
 
-function Login({ setLoginOpen, userStatus, setUserStatus, loginOpen }) {
+// TODO: make sure username and email are unique on signup. create logic for inviting admin
+// TODO: make signup one page, make fields "required" if needed
+
+function Login() {
   const location = useLocation();
   const { login, triggerLoading } = useUser();
+  const { setLoginOpen, userStatus, setUserStatus } = useLogin();
   const navigate = useNavigate();
 
-  const [signupPopup, setSignupPopup] = useState(false);
+  const [onboardingStatus, setOnboardingStatus] = useState("Register");
+  const [allCommunities, setAllCommunities] = useState([]);
+  const [allTractates, setAllTractates] = useState([]);
   const [userData, setUserData] = useState({
     email: "",
-    username: "",
+    // username: "",
     password: "",
     password_confirmation: "",
     phone_number: "",
     first_name: "",
     last_name: "",
     location: "",
+  });
+  const [optionalUserData, setOptionalUserData] = useState({
+    community: null,
+    tractate: null,
   });
 
   useEffect(
@@ -38,6 +43,7 @@ function Login({ setLoginOpen, userStatus, setUserStatus, loginOpen }) {
       }
 
       if (location.pathname.includes("/adminInvite")) {
+        // ? make login a page and include community ID in route
         // TODO: set up logic to make user admin
         // if user exists, log them in, else sign them up
       }
@@ -45,44 +51,56 @@ function Login({ setLoginOpen, userStatus, setUserStatus, loginOpen }) {
     [location.pathname]
   );
 
+  useEffect(
+    function getAllDataForOnboarding() {
+      if (
+        onboardingStatus === "selectCommunity" &&
+        allCommunities.length <= 0
+      ) {
+        const fetchCommunities = async () => {
+          try {
+            const communities = await getCommunities();
+            setAllCommunities(communities);
+          } catch (err) {
+            console.log(err);
+            alert("Failed to load communities");
+          }
+        };
+        fetchCommunities();
+      }
+
+      if (onboardingStatus === "selectTractate" && allTractates.length <= 0) {
+        const fetchTractates = async () => {
+          try {
+            const tractates = await getAllTractates();
+            setAllTractates(tractates);
+          } catch (err) {
+            alert("Failed to load tractates");
+          }
+        };
+        fetchTractates();
+      }
+    },
+    [onboardingStatus]
+  );
+
   const handleChange = (e) => {
-    setUserData({ ...userData, [e.target.name]: e.target.value });
+    const { name, value } = e.target;
+    if (name === "community" || name === "tractate") {
+      setOptionalUserData({ ...optionalUserData, [name]: value });
+    } else {
+      setUserData({ ...userData, [name]: value });
+    }
   };
 
-  const handleSignup = () => {
-    if (userData.password !== userData.password_confirmation) {
-      alert("Passwords do not match");
-      return;
-    }
-    if (userData.email === "") {
-      alert("Please enter an email");
-      return;
-    }
-    if (userData.password === "" || userData.password_confirmation === "") {
-      alert("Please enter a password");
-      return;
-    }
-    setSignupPopup(true);
-    // setLoginOpen(false);
-  };
-
-  const handleFormToggle = () => {
-    setUserStatus(userStatus === "Login" ? "Signup" : "Login");
-  };
-
-  const handleSubmit = async (e) => {
-    if (e === "Login") {
-      try {
-        await login(userData.username, userData.password);
-        triggerLoading();
-        setLoginOpen(false);
-        navigate(location.pathname);
-      } catch (err) {
-        alert("Invalid credentials");
+  const handleSignupFields = () => {
+    // ! username was taken out but needs to be added until backend is updated
+    if (userStatus === "Signup") {
+      if (userData.password !== userData.password_confirmation) {
+        alert("Passwords do not match");
         return;
       }
-    } else if (e === "Register user") {
-      // handleSignup();
+
       let missingFields = [];
 
       // Collect all the missing fields
@@ -106,16 +124,47 @@ function Login({ setLoginOpen, userStatus, setUserStatus, loginOpen }) {
         alert("Please enter a valid phone number");
         return;
       }
-      console.log("Registering user", userData);
-      // navigate("/profile");
+      setOnboardingStatus("selectCommunity");
+    }
+  };
+
+  const handleFormToggle = () => {
+    setUserStatus(userStatus === "Login" ? "Signup" : "Login");
+  };
+
+  const handleSubmit = async (e) => {
+    if (e === "Login") {
+      if (userData.username === "" || userData.password === "") {
+        alert("Fields cannot be empty");
+      } else {
+        try {
+          await login(userData.username, userData.password);
+          triggerLoading();
+          setLoginOpen(false);
+          navigate(location.pathname);
+        } catch (err) {
+          alert("Invalid credentials");
+          return;
+        }
+      }
+    } else if (e === "Register user") {
+      // TODO: need a way to check if username or email exist (before user can click next)
+      // ? fetch that checks if username or email exist in DB?
+      // TODO: need to create user, then set their community if one was selected, then create goal if it was selected
       try {
         const user = await createUser(userData);
-        console.log("User created", user);
-        if (user === "Account successfully created") {
-          navigate("/community");
-        } else {
-          alert("Error creating user");
+        try {
+          // update user (and user state?) to add community
+          // ? can i add community and/or goal upon register or do i need to update user after?
+        } catch (err) {
+          alert("Error connecting to a community");
         }
+        // console.log("User created", user);
+        // if (user === "Account successfully created") {
+        navigate("/community");
+        // } else {
+        // alert("Error creating user");
+        // }
       } catch (err) {
         console.error("Error creating user", err);
         alert("Error creating user");
@@ -133,260 +182,68 @@ function Login({ setLoginOpen, userStatus, setUserStatus, loginOpen }) {
         borderColor: "black",
       },
       "&:hover fieldset": {
-        borderColor: "var(--orange-light)",
+        borderColor: "var(--light-grey)",
       },
       "&.Mui-focused fieldset": {
-        borderColor: "var(--orange)",
+        borderColor: "var(--black)",
       },
     },
     "& .MuiInputLabel-root": {
       color: "black",
     },
-    "& .MuiSelect-select": {
-      color: "var(--orange)",
-    },
   };
 
   return (
-    <Dialog
-      open={loginOpen}
-      className="popup-overlay"
-      onClose={() => setLoginOpen(false)}
-      PaperComponent={Card}
+    <div
+      className="container"
+      style={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "flex-start",
+        backgroundColor: "white",
+      }}
     >
-      {!signupPopup && (
-        <div
-          className="popup-card"
+      <div
+        className="loginAndSignupCard"
+        style={{
+          textAlign: "center",
+          width: "100%",
+          maxWidth: "400px",
+        }}
+      >
+        <Button
+          onClick={() => {
+            setLoginOpen(false)
+            setUserStatus("Login")
+          }}
           style={{
-            position: "relative",
-            height: "30em",
-            width: "22em",
+            position: "absolute",
+            top: "30px",
+            right: "10px",
+            boxShadow: "none",
           }}
         >
-          <Button
-            onClick={() => setLoginOpen(false)}
-            style={{ position: "absolute", top: "10px", right: "10px" }}
-          >
-            <CloseRoundedIcon style={{ color: "var(--orange)" }} />
-          </Button>
-          <article className="input" style={{ marginTop: "10px" }}>
-            {userStatus === "Login" && (
-              <form
-                onSubmit={(e) => {
-                  e.preventDefault();
-                  handleSubmit("Login");
-                }}
-              >
-                <label>Login</label>
-                <TextField
-                  id="username"
-                  label="Username"
-                  variant="outlined"
-                  type="text"
-                  name="username"
-                  value={userData.username}
-                  onChange={handleChange}
-                  sx={{
-                    marginTop: "2em",
-                    ...cardStyle,
-                  }}
-                />
-                <TextField
-                  id="password"
-                  label="Password"
-                  variant="outlined"
-                  type="password"
-                  name="password"
-                  value={userData.password}
-                  onChange={handleChange}
-                  sx={{
-                    ...cardStyle
-                  }}
-                />
-                <Button
-                  type="submit"
-                  variant="contained"
-                  style={{ backgroundColor: "var(--orange)", width: "90%" }}
-                >
-                  Login
-                </Button>
-              </form>
-            )}
-
-            {userStatus === "Signup" && (
-              <div>
-                <form
-                  onSubmit={(e) => {
-                    e.preventDefault();
-                    // handleSubmit("Register user");
-                    handleSignup();
-                  }}
-                >
-                  <label>Signup</label>
-                  <TextField
-                    id="email"
-                    label="Email"
-                    variant="outlined"
-                    type="text"
-                    name="email"
-                    value={userData.email}
-                    onChange={handleChange}
-                    sx={{
-                      marginTop: "2em",
-                      ...cardStyle
-                    }}
-                  />
-                  <TextField
-                    id="password"
-                    label="Password"
-                    variant="outlined"
-                    type="password"
-                    name="password"
-                    value={userData.password}
-                    onChange={handleChange}
-                    sx={{
-                      ...cardStyle
-                    }}
-                  />
-                  <TextField
-                    id="confirm-password"
-                    label="Confirm Password"
-                    variant="outlined"
-                    type="password"
-                    name="password_confirmation"
-                    value={userData.password_confirmation}
-                    onChange={handleChange}
-                    sx={{
-                      ...cardStyle
-                    }}
-                  />
-                  <Button
-                    type="submit"
-                    variant="contained"
-                    style={{ backgroundColor: "var(--orange)", width: "90%" }}
-                  >
-                    Signup
-                  </Button>
-                </form>
-              </div>
-            )}
-
-            <Button
-              type="button"
-              onClick={handleFormToggle}
-              style={{
-                color: "var(--orange)",
-                position: "absolute",
-                bottom: "10px",
-                right: "10px",
-              }}
-            >
-              {userStatus === "Login"
-                ? "Create an account"
-                : "Already have an account?"}
-            </Button>
-          </article>
-        </div>
-      )}
-      {signupPopup && (
-        <div
-          className="popup-card"
-          style={{ position: "relative", height: "30em", width: "22em" }}
-        >
-          <Button
-            onClick={() => {
-              setSignupPopup(false);
-              setLoginOpen(true);
-            }}
-            style={{ position: "absolute", top: "10px", right: "10px" }}
-          >
-            <CloseRoundedIcon style={{ color: "var(--orange)" }} />
-          </Button>
-          <form
-            onSubmit={(e) => {
-              e.preventDefault();
-              handleSubmit("Register user");
-            }}
-          >
-            <label>Register</label>
-            <TextField
-              id="username"
-              label="Username"
-              variant="outlined"
-              type="text"
-              name="username"
-              value={userData.username}
-              onChange={handleChange}
-              sx={{
-                marginTop: "2em",
-              ...cardStyle
-              }}
-            />
-            <TextField
-              id="first-name"
-              label="First Name"
-              variant="outlined"
-              type="text"
-              name="first_name"
-              value={userData.first_name}
-              onChange={handleChange}
-              sx={{
-              ...cardStyle
-              }}
-            />
-            <TextField
-              id="last-name"
-              label="Last Name"
-              variant="outlined"
-              type="text"
-              name="last_name"
-              value={userData.last_name}
-              onChange={handleChange}
-              sx={{
-              ...cardStyle
-              }}
-            />
-            <TextField
-              id="phone-number"
-              label="Phone Number"
-              variant="outlined"
-              type="tel"
-              name="phone_number"
-              value={userData.phone_number}
-              onChange={handleChange}
-              sx={{
-              ...cardStyle
-              }}
-            />
-            <TextField
-              id="outlined-select-state"
-              select
-              label="State"
-              name="location"
-              value={userData.location}
-              onChange={handleChange}
-              defaultValue="Select state"
-              sx={{
-              ...cardStyle
-              }}
-            >
-              {states.map((state, index) => (
-                <MenuItem key={index} value={state}>
-                  {state}
-                </MenuItem>
-              ))}
-            </TextField>
-            <Button
-              type="submit"
-              variant="contained"
-              style={{ backgroundColor: "var(--orange)", width: "90%" }}
-            >
-              Create Account
-            </Button>
-          </form>
-        </div>
-      )}
-    </Dialog>
+          <CloseRoundedIcon style={{ color: "black" }} />
+        </Button>
+        <LoginPopup
+          handleSignupFields={handleSignupFields}
+          handleSubmit={handleSubmit}
+          userData={userData}
+          handleChange={handleChange}
+          handleFormToggle={handleFormToggle}
+          cardStyle={cardStyle}
+          onboardingStatus={onboardingStatus}
+          setOnboardingStatus={setOnboardingStatus}
+          allCommunities={allCommunities}
+          optionalUserData={optionalUserData}
+        />
+      </div>
+    </div>
   );
 }
 
