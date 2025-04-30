b53aa6e9c4cd46482efbc335e72cea5ffc2ba8b0
Benny Sufrin <bennysuf@gmail.com>
2025-04-30 17:06:28 -0400

    Remove cards, add Liveupdate component, and update styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,25 +2,18 @@ import {
   Box,
   Typography,
   Button,
-  Card,
-  CardContent,
-  CardActionArea,
-  Grid,
   TaskAltRoundedIcon,
-  Slider,
 } from "../utils/dataExports/muiExports";
 import { useNavigate } from "react-router-dom";
 import { useUser, useLogin } from "../utils/Context";
 import Logo from "../components/Logo";
 import Nav from "../components/Nav";
+import LiveUpdates from "../components/LiveUpdates";
 
 function Home() {
   const navigate = useNavigate();
   const { isAuth } = useUser();
   const { setLoginOpen, setUserStatus } = useLogin();
-
-  // get communities and display first 3
-  const cards = [{ id: 1 }, { id: 2 }, { id: 3 }];
 
   return (
     <Box style={{ marginTop: "3em" }}>
@@ -29,28 +22,34 @@ function Home() {
       <Box
         className="pageDisplay"
         sx={{
-          width: "100%",
+          paddingLeft: "5%",
+          paddingRight: "5%",
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
-          flexDirection: { xs: "column", md: "row" },
+          flexDirection: "column",
           paddingTop: { xs: "2em", md: "50px" },
         }}
       >
-        <Box>
-          <Typography variant="h4" sx={{ color: "var(--black)" }}>
+        <Box sx={{ color: "var(--brown)" }}>
+          <Typography
+            sx={{
+              fontFamily: "Nexa, sans-serif",
+              fontSize: "2rem",
+              height: "35px",
+            }}
+          >
             United through learning
           </Typography>
-          <Typography variant="h4" sx={{ color: "var(--black)" }}>
+          <Typography sx={{ fontFamily: "Nexa, sans-serif", fontSize: "2.1rem" }}>
             Chalukas Hashas
           </Typography>
           <Typography
-            variant="overline"
             sx={{
               display: "flex",
               alignItems: "center",
-              fontSize: "0.5em",
-              marginTop: "1em",
+              fontSize: ".7rem",
+              marginTop: ".5em",
             }}
           >
             <TaskAltRoundedIcon
@@ -59,11 +58,11 @@ function Home() {
             Track your progress
           </Typography>
           <Typography
-            variant="overline"
             sx={{
               display: "flex",
               alignItems: "center",
-              fontSize: "0.5em",
+              fontSize: ".7rem",
+              marginTop: ".3em",
             }}
           >
             <TaskAltRoundedIcon
@@ -72,17 +71,17 @@ function Home() {
             Connect with a Chavrusa
           </Typography>
           <Typography
-            variant="overline"
             sx={{
               display: "flex",
               alignItems: "center",
-              fontSize: "0.5em",
+              fontSize: ".7rem",
+              marginTop: ".3em",
             }}
           >
             <TaskAltRoundedIcon
               sx={{ marginRight: "7px", color: "var(--orange)" }}
             />{" "}
-            Explore community events
+            Explore Community Events
           </Typography>
           {!isAuth && (
             <Button
@@ -91,7 +90,10 @@ function Home() {
               sx={{
                 boxShadow: "none",
                 marginTop: "20px",
-                backgroundColor: "var(--black)",
+                height: "3rem",
+                width: "5.5rem",
+                fontSize: ".7em",
+                backgroundColor: "var(--brown)",
                 borderRadius: "10px",
                 textTransform: "none",
               }}
@@ -107,9 +109,12 @@ function Home() {
             variant="contained"
             sx={{
               boxShadow: "none",
+              height: "3rem",
+              width: "11.5rem",
+              fontSize: ".7em",
               marginTop: "20px",
               backgroundColor: "var(--orange)",
-              marginLeft: "25px",
+              marginLeft: "15px",
               borderRadius: "10px",
               textTransform: "none",
             }}
@@ -117,167 +122,8 @@ function Home() {
           >
             View Communities
           </Button>
-        </Box>{" "}
-        <Card
-          variant="outlined"
-          sx={{
-            height: "15em",
-            width: "20em",
-            marginLeft: { xs: "0px", md: "100px" },
-            marginTop: { xs: "25px", md: "0px" },
-          }}
-        >
-          Images
-        </Card>
-      </Box>
-      <Box className="communityCardPreview">
-        <Grid
-          container
-          direction="row"
-          sx={{
-            justifyContent: "center",
-            alignItems: "flex-end",
-          }}
-        >
-          {cards.map((card) => (
-            <Card
-              key={card.id}
-              variant="outlined"
-              sx={{
-                border: "2px solid var(--light-grey)",
-                borderRadius: "16px",
-                flex: "1 1 200px",
-                minWidth: "100px",
-                maxWidth: "300px",
-                height: "150px",
-                marginRight: "1em",
-                marginLeft: "1em",
-                marginTop: "1em",
-              }}
-            >
-              <CardActionArea
-                sx={{
-                  height: "100%",
-                  "&[data-active]": {
-                    backgroundColor: "action.selected",
-                    "&:hover": {
-                      backgroundColor: "action.selectedHover",
-                    },
-                  },
-                }}
-              >
-                <CardContent
-                  sx={{
-                    height: "100%",
-                    display: "flex",
-                    alignItems: "center",
-                  }}
-                >
-                  <img src={card.image} alt="Community" />
-                  <div>
-                    <Typography variant="body2" component="div">
-                      Chalukas Hashas
-                    </Typography>
-                    <Typography variant="caption" color="text.secondary">
-                      community name Community
-                    </Typography>
-                    <br />
-                    <br />
-                    <Box
-                      className="progressBarContainer"
-                      sx={{
-                        display: "flex",
-                        alignItems: "center",
-                        margin: "10px",
-                      }}
-                    >
-                      <Box sx={{ width: "100%", mr: 1 }}>
-                        <Slider
-                          disabled
-                          defaultValue={30}
-                          // value={
-                          //   community.community_goal?.length > 0
-                          //     ? (community.community_goal[0]
-                          //         .community_total_completed_pages /
-                          //         community.community_goal[0]
-                          //           .community_total_selected_pages) *
-                          //       100
-                          //     : 0
-                          // }
-                          aria-label="Disabled slider"
-                          sx={{
-                            "& .MuiSlider-thumb": {
-                              color: "var(--light-blue)",
-                              height: "12px",
-                              width: "12px",
-                            },
-                            "& .MuiSlider-track": {
-                              color: "var(--light-blue)",
-                            },
-                            "& .MuiSlider-rail": {
-                              color: "var(--light-grey)",
-                            },
-                          }}
-                        />
-                      </Box>
-                      <Box sx={{ minWidth: 35 }}>
-                        <Typography variant="body2" color="text.secondary">
-                          {"30%"}
-                          {/* {` ${community.community_goal?.length > 0
-                                    ? (
-                                        parseFloat(
-                                          community.community_goal[0]
-                                            .community_total_completed_pages /
-                                            community.community_goal[0]
-                                              .community_total_selected_pages
-                                        ) * 100
-                                      ).toFixed(2)
-                                    : 0}%`} */}
-                        </Typography>
-                      </Box>
-                    </Box>
-                  </div>
-                </CardContent>
-              </CardActionArea>
-            </Card>
-          ))}
-          {/* <Card
-            sx={{
-              flex: "1 1 200px",
-              minWidth: "150px",
-              maxWidth: "360px",
-              height: "150px",
-              backgroundColor: "var(--orange)",
-            }}
-          >
-            <CardActionArea
-              onClick={() => navigate("/community")}
-              sx={{
-                height: "100%",
-                "&[data-active]": {
-                  backgroundColor: "action.selected",
-                  "&:hover": {
-                    backgroundColor: "action.selectedHover",
-                  },
-                },
-              }}
-            >
-              <CardContent>
-                <Typography
-                  variant="h5"
-                  component="div"
-                  style={{
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "center",
-                  }}
-                >
-                  View more <EastRoundedIcon />
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card> */}
-        </Grid>
+        </Box>
+        <LiveUpdates />
       </Box>
     </Box>
   );
