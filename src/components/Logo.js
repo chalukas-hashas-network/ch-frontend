2d846610678158cc970c60ca182a86c649ef5ac7
Benny Sufrin <bennysuf@gmail.com>
2025-05-02 16:46:52 -0400

    update image and styling

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,14 +1,20 @@
 import { Typography } from "../utils/dataExports/muiExports";
-import logo from "../assets/images/Chalukas Hashas Logo ts.png";
-
+import logo from "../assets/images/Chalukas Hashas Logo TS no bottoms text .png";
 
 function Logo({ community }) {
   return (
     <div
       style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
     >
-      <img src={logo} style={{ height: "100px", width: "auto" }} alt="Logo" />
-      <Typography variant="overline" sx={{ color: "var(--orange)" }}>
+      <img src={logo} style={{ height: "120px", width: "auto" }} alt="Logo" />
+      <Typography
+        sx={{
+          color: "var(--orange)",
+          fontFamily: "Nexa, sans-serif",
+          fontSize: ".9em",
+          paddingTop: ".2rem",
+        }}
+      >
         {community.toUpperCase()} COMMUNITY
       </Typography>
     </div>
