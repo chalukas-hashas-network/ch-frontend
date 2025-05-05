a4da90bcd3afdfe4424cb56c05af30c3d05de642
Benny Sufrin <bennysuf@gmail.com>
2025-05-05 17:34:44 -0400

    Add theme provider and custom text field styles

--- src/index.js
+++ src/index.js
@@ -5,20 +5,24 @@ import reportWebVitals from "./reportWebVitals.js";
 import { LoginProvider } from "./utils/context/LoginContext.js";
 import { UserProvider } from "./utils/context/UserContext.js";
 import { CommunityProvider } from "./utils/context/CommunityContext.js";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./utils/context/ThemeContext.js";
 import React from "react";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <CommunityProvider>
-      <UserProvider>
-        <LoginProvider>
-          <BrowserRouter>
-            <App />
-          </BrowserRouter>
-        </LoginProvider>
-      </UserProvider>
-    </CommunityProvider>
+    <ThemeProvider theme={theme}>
+      <CommunityProvider>
+        <UserProvider>
+          <LoginProvider>
+            <BrowserRouter>
+              <App />
+            </BrowserRouter>
+          </LoginProvider>
+        </UserProvider>
+      </CommunityProvider>
+    </ThemeProvider>
   </React.StrictMode>
 );
 
---
+++ src/utils/context/ThemeContext.js
@@ -0,0 +1,37 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: `'Cerebri Sans', sans-serif`,
+  },
+  components: {
+    MuiTextField: {
+      styleOverrides: {
+        root: {
+          width: '90%',
+          marginBottom: '1em',
+          '& .MuiOutlinedInput-root': {
+            borderRadius: '10px',
+            '& fieldset': {
+              borderColor: 'var(--black)',
+            },
+            '&:hover fieldset': {
+              borderColor: 'var(--dark-grey)',
+            },
+            '&.Mui-focused fieldset': {
+              borderColor: 'var(--black)',
+            },
+          },
+          '& .MuiInputLabel-root': {
+            color: 'var(--brown)',
+          },
+          '& .MuiSelect-select': {
+            color: 'var(--black)',
+          },
+        },
+      },
+    },
+  },
+});
+
+export default theme;
