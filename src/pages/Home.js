611861c51c85227749009396843e4785bc11ccf8
Benny Sufrin <bennysuf@gmail.com>
2025-05-09 17:48:09 -0400

    move boxes into own component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,6 +6,7 @@ import Logo from "../components/Logo";
 import Nav from "../components/Nav";
 import LiveUpdates from "../components/LiveUpdates";
 import check from "../assets/images/Check icon Chalukas .png";
+import DataBoxesDisplay from "../components/DataBoxesDisplay";
 
 function Home() {
   const navigate = useNavigate();
@@ -180,136 +181,7 @@ function Home() {
           )}
         </Box>
         <LiveUpdates />
-        <Box
-          className="dataBoxesDisplay"
-          sx={{
-            display: "flex",
-            flexDirection: "row",
-            gap: 2,
-            marginTop: "1.5rem",
-          }}
-        >
-          <Box
-            sx={{
-              backgroundColor: "var(--dark-grey)",
-              borderRadius: "16px",
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "flex-start",
-              alignItems: "center",
-              position: "relative",
-              width: "7rem",
-              height: "4.8rem",
-              boxSizing: "border-box",
-            }}
-          >
-            <Typography
-              sx={{
-                fontSize: ".8em",
-                color: "var(--brown)",
-                padding: ".5em",
-              }}
-            >
-              Members
-            </Typography>
-            <Box
-              sx={{
-                borderRadius: "13px",
-                bgcolor: "white",
-                width: "90%",
-                height: "55%",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                marginBottom: "5px",
-              }}
-            >
-              <Typography sx={{ color: "var(--light-blue)", fontSize: ".8em" }}>
-                226
-              </Typography>
-            </Box>
-          </Box>
-          <Box
-            sx={{
-              backgroundColor: "var(--dark-grey)",
-              borderRadius: "16px",
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "flex-start",
-              alignItems: "center",
-              position: "relative",
-              width: "7rem",
-              height: "4.8rem",
-              boxSizing: "border-box",
-            }}
-          >
-            <Typography
-              sx={{
-                fontSize: ".8em",
-                color: "var(--brown)",
-                padding: ".5em",
-              }}
-            >
-              Pages
-            </Typography>
-            <Box
-              sx={{
-                borderRadius: "13px",
-                bgcolor: "white",
-                width: "90%",
-                height: "55%",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                marginBottom: "5px",
-              }}
-            >
-              <Typography sx={{ color: "var(--light-blue)", fontSize: ".8em" }}>
-                306,983
-              </Typography>
-            </Box>
-          </Box>
-          <Box
-            sx={{
-              backgroundColor: "var(--dark-grey)",
-              borderRadius: "16px",
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "flex-start",
-              alignItems: "center",
-              position: "relative",
-              width: "7rem",
-              height: "4.8rem",
-              boxSizing: "border-box",
-            }}
-          >
-            <Typography
-              sx={{
-                fontSize: ".8em",
-                color: "var(--brown)",
-                padding: ".5em",
-              }}
-            >
-              Events
-            </Typography>
-            <Box
-              sx={{
-                borderRadius: "13px",
-                bgcolor: "white",
-                width: "90%",
-                height: "55%",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                marginBottom: "5px",
-              }}
-            >
-              <Typography sx={{ color: "var(--light-blue)", fontSize: ".8em" }}>
-                42
-              </Typography>
-            </Box>
-          </Box>
-        </Box>
+        <DataBoxesDisplay />
       </Box>
     </Box>
   );
