4afa573ec85966c61cb5d9533e6a0a15964d1873
Benny Sufrin <bennysuf@gmail.com>
2025-05-12 15:09:56 -0400

    add a get all communities fetch for Community list preventing a new fetch on page change, add useMemo preventing refetch if no change was made

--- src/utils/context/CommunityContext.js
+++ src/utils/context/CommunityContext.js
@@ -1,20 +1,53 @@
-import {useState, useContext, createContext} from "react"
+import { useState, useContext, createContext, useMemo, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import { useUser } from "./UserContext";
+import { getCommunities } from "../API/CommunityAPI";
 
 const CommunityContext = createContext();
 
-export const CommunityProvider = ({children}) => {
+export const CommunityProvider = ({ children }) => {
+  const [allCommunities, setAllCommunities] = useState([]);
 
-    return (
-        <CommunityContext.Provider
-        value={{
-            
-        }}
-        >
-            {children}
-        </CommunityContext.Provider>
-    )
-}
+  const location = useLocation();
+  const { setIsLoading } = useUser();
+
+  const getAllCommunityData = async () => {
+    // gets all communities for community page without causing rerender on page load
+
+    try {
+      const communities = await getCommunities({}, [
+        "community_goal",
+        "members",
+      ]);
+      setIsLoading(true);
+      setAllCommunities(communities);
+      console.log("triggered community fetch");
+    } catch (err) {
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    const shouldFetch =
+      location.pathname.includes("/community") && allCommunities.length === 0;
+
+    if (shouldFetch) {
+      getAllCommunityData();
+    }
+  }, [location.pathname]);
+
+  const value = useMemo(() => {
+    return allCommunities;
+  }, [allCommunities]);
+
+  return (
+    <CommunityContext.Provider value={value}>
+      {children}
+    </CommunityContext.Provider>
+  );
+};
 
 export const useCommunity = () => {
-    return useContext(CommunityContext)
-}
+  return useContext(CommunityContext);
+};
