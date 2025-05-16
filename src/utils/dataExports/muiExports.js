be0903600fee045afa93fbc91a51d797dd5de2a0
Benny Sufrin <bennysuf@gmail.com>
2025-05-16 14:48:56 -0400

    remove unused import and add new ones

--- src/utils/dataExports/muiExports.js
+++ src/utils/dataExports/muiExports.js
@@ -1,25 +1,25 @@
+import AccountCircleRoundedIcon from "@mui/icons-material/AccountCircleRounded";
 import Autocomplete from "@mui/material/Autocomplete";
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-import Avatar from '@mui/material/Avatar';
-import Box from '@mui/material/Box';
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import Avatar from "@mui/material/Avatar";
+import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
 import CardActionArea from "@mui/material/CardActionArea";
 import CardContent from "@mui/material/CardContent";
-import CircularProgress from '@mui/material/CircularProgress';
-import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
+import CircularProgress from "@mui/material/CircularProgress";
+import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
 import Container from "@mui/material/Container";
-import DeleteIcon from '@mui/icons-material/Delete';
+import DeleteIcon from "@mui/icons-material/Delete";
 import Dialog from "@mui/material/Dialog";
 import Divider from "@mui/material/Divider";
 import Drawer from "@mui/material/Drawer";
-import EastRoundedIcon from "@mui/icons-material/EastRounded";
 import Grid from "@mui/material/Grid2";
 import IconButton from "@mui/material/IconButton";
-import LinearProgress from "@mui/material/LinearProgress";
+import KeyboardArrowDownRoundedIcon from "@mui/icons-material/KeyboardArrowDownRounded";
 import List from "@mui/material/List";
-import ListItem from '@mui/material/ListItem';
-import ListItemAvatar from '@mui/material/ListItemAvatar';
+import ListItem from "@mui/material/ListItem";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemText from "@mui/material/ListItemText";
 import Menu from "@mui/material/Menu";
@@ -27,11 +27,11 @@ import MenuIcon from "@mui/icons-material/Menu";
 import MenuItem from "@mui/material/MenuItem";
 import NoteAddIcon from "@mui/icons-material/NoteAdd";
 import Paper from "@mui/material/Paper";
-import PersonAddAltRoundedIcon from '@mui/icons-material/PersonAddAltRounded';
-import Select from '@mui/material/Select';
-import Slider from '@mui/material/Slider';
+import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
+import Select from "@mui/material/Select";
+import Slider from "@mui/material/Slider";
 import SpeedDial from "@mui/material/SpeedDial";
-import SpeedDialAction from "@mui/material/SpeedDialAction"
+import SpeedDialAction from "@mui/material/SpeedDialAction";
 import SpeedDialIcon from "@mui/material/SpeedDialIcon";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
@@ -40,7 +40,8 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TablePagination from "@mui/material/TablePagination";
 import TableRow from "@mui/material/TableRow";
-import TaskAltRoundedIcon from "@mui/icons-material/TaskAltRounded";
+import Tab from "@mui/material/Tab";
+import Tabs, { tabsClasses } from "@mui/material/Tabs";
 import TextField from "@mui/material/TextField";
 import ToggleButton from "@mui/material/ToggleButton";
 import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
@@ -48,6 +49,7 @@ import Typography from "@mui/material/Typography";
 import UpgradeIcon from "@mui/icons-material/Upgrade";
 
 export {
+  AccountCircleRoundedIcon,
   Autocomplete,
   ArrowBackIcon,
   Avatar,
@@ -63,10 +65,9 @@ export {
   Dialog,
   Divider,
   Drawer,
-  EastRoundedIcon,
   Grid,
   IconButton,
-  LinearProgress,
+  KeyboardArrowDownRoundedIcon,
   List,
   ListItem,
   ListItemAvatar,
@@ -77,7 +78,7 @@ export {
   MenuItem,
   NoteAddIcon,
   Paper,
-  PersonAddAltRoundedIcon,
+  SearchRoundedIcon,
   Select,
   Slider,
   SpeedDial,
@@ -90,7 +91,9 @@ export {
   TableHead,
   TablePagination,
   TableRow,
-  TaskAltRoundedIcon,
+  Tab,
+  Tabs,
+  tabsClasses,
   TextField,
   ToggleButton,
   ToggleButtonGroup,
