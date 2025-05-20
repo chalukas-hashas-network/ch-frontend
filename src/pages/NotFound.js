c7a787461e86636c79ef1418fae1e15a03d18feb
Benny Sufrin <bennysuf@gmail.com>
2025-05-20 13:47:11 -0400

    create UI for page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,15 +1,41 @@
-import { Link } from "react-router-dom";
+import { Button, Typography } from "../utils/dataExports/muiExports";
+import { useNavigate } from "react-router-dom";
+
 function NotFound() {
+  const navigate = useNavigate();
   return (
-    <div>
-      <h1>404</h1>
-      <br />
-      <h3>Uh Oh! You are lost.</h3>
-      <br/>
-      <p>This page you are looking for does not exist.</p>
-      <Link style={{ textDecoration: "none", color: "blue" }} to="/home">
-        {"HOME"}
-      </Link>
+    <div style={{ textAlign: "center", marginTop: "5em" }}>
+      <Typography sx={{ fontSize: "4em", color: "var(--brown)" }}>
+        404
+      </Typography>
+      <Typography sx={{ fontSize: "2em", marginTop: ".7em" }}>
+        Uh Oh! You are lost.
+      </Typography>
+      <Typography
+        sx={{ fontSize: "1.2em", color: "var(--brown)", marginTop: ".5em" }}
+      >
+        The page you are looking for does not exist, try looking in a different
+        Mesechta.
+      </Typography>
+      <Button
+        variant="contained"
+        onClick={() => {
+          navigate("/home");
+        }}
+        sx={{
+          borderRadius: "13px",
+          boxShadow: "none",
+          backgroundColor: "var(--orange)",
+          fontWeight: "300",
+          fontSize: "0.8em",
+          width: "10em",
+          height: "3em",
+          marginTop: "3em",
+          textTransform: "none",
+        }}
+      >
+        Take me Home
+      </Button>
     </div>
   );
 }
